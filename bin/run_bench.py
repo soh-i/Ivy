821d6870f4715d73b4a216782368115005c89559
yukke <soh.ishiguro3914@gmail.com>
2013-10-18 01:32:45 +0900

    fix

--- bin/run_bench.py
+++ bin/run_bench.py
@@ -2,7 +2,7 @@ from Ivy.benchmark import *
 
 if __name__ == '__main__':
     darned_reader = DarnedReader(sp='human')
-    v = VCFReader('/home/soh.i/RNA_editing/sandbox/rec3h_205_reed0_mis2gap0dist0.vcf')
+    v = VCFReader('../data/test_data.vcf')
     pred = v.generate_vcf_set()
     ans = darned_reader.generate_darned_set()
     bench = Benchmark(answer=ans, predict=pred)
