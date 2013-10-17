130bc362368d74e29374082961e96b0219989aa5
yukke <soh.ishiguro3914@gmail.com>
2013-10-17 21:45:34 +0900

    update sample script

--- bin/run_bench.py
+++ bin/run_bench.py
@@ -1,10 +1,17 @@
-from Ivy.benchmark import Benchmark
+from Ivy.benchmark import *
 
 if __name__ == '__main__':
-    ans = [291, 393, 93, 33, 1, 3, 4,6,0]
-    pred = [9,32,1,99,93, 939, 10]
+    darned_reader = DarnedReader(sp='human')
+    v = VCFReader('/home/soh.i/RNA_editing/sandbox/rec3h_205_reed0_mis2gap0dist0.vcf')
+    pred = v.generate_vcf_set()
+    ans = darned_reader.generate_darned_set()
     bench = Benchmark(answer=ans, predict=pred)
-    
-    print bench.recall()
     print bench.precision()
+    print bench.recall()
     print bench.f_measure()
+
+    
+
+    
+    
+    
