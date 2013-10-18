a835ca0063d69702b8004d377f06e686e18bd4b6
yukke <soh.ishiguro3914@gmail.com>
2013-10-18 21:01:19 +0900

    fix

--- bin/run_bench.py
+++ bin/run_bench.py
@@ -1,16 +1,15 @@
 from Ivy.benchmark import *
 
 if __name__ == '__main__':
-    darned_reader = DarnedReader(sp='human')
+    darned_reader = DarnedReader(sp='human_hg19')
+    print darned_reader
     v = VCFReader('../data/test_data.vcf')
     pred = v.generate_vcf_set()
     ans = darned_reader.generate_darned_set()
     bench = Benchmark(answer=ans, predict=pred)
 
-    print darned_reader.db_name()
-    print darned_reader.path()
     
-    print "Precision:\t%f" % (bench.precision())
-    print "Recall:\t%f" % (bench.recall())
-    print "F-measure:\t%f" % (bench.f_measure())
+    #print "Precision:\t%f" % (bench.precision())
+    #print "Recall:\t%f" % (bench.recall())
+    #print "F-measure:\t%f" % (bench.f_measure())
 
