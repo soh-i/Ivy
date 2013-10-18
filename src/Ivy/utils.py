565b39a00ed0f156cc82e7c6f621aabc6bcdb9cf
yukke <soh.ishiguro3914@gmail.com>
2013-10-18 21:01:09 +0900

    Peel off the class to global modules

--- src/Ivy/utils.py
+++ src/Ivy/utils.py
@@ -2,15 +2,11 @@ import ConfigParser
 import os
 import sys
 
-class DataConfig(object):
-    def __init__(self):
-        pass
+def find_app_root():
+    root = os.path.dirname(__file__)
+    while not os.path.exists(os.path.join(root, 'setup.py')):
+        root = os.path.abspath(os.path.join(root, os.path.pardir))
+    return root
 
-    def find_app_root(self):
-        root = os.path.dirname(__file__)
-        while not os.path.exists(os.path.join(root, 'setup.py')):
-            root = os.path.abspath(os.path.join(root, os.path.pardir))
-        return root
-
-        
-        
+if __name__ == '__main__':
+    print find_app_root()
