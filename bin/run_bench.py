0dccd6e007070e0281a78116b99ff5e5eee417de
yukke <soh.ishiguro3914@gmail.com>
2013-10-21 15:57:10 +0900

    Add --sp option and remove --verbose option

--- bin/run_bench.py
+++ bin/run_bench.py
@@ -3,7 +3,7 @@ import argparse
 
 if __name__ == '__main__':
     version = '0.0.1'
-    desc = "Benchmarking test for detected RNA editing sites based on HTSeq data"
+    desc = "Benchmarking test for detected RNA editing sites based on HTSeq data."
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--vcf',
                         required=True,
@@ -11,11 +11,17 @@ if __name__ == '__main__':
                         action='store',
                         help='set VCF file [required]'
                     )
+    parser.add_argument('--sp',
+                        required=True,
+                        dest='species',
+                        action='store',
+                        help='set species name and genome version, ex: human_hg19 [required]'
+                    )
     parser.add_argument('--db',
                         required=False,
                         dest='db_file',
                         action='store',
-                        help='set db name'
+                        help='set db name [not implemented]'
                     )
     parser.add_argument('--config',
                         required=False,
@@ -23,17 +29,11 @@ if __name__ == '__main__':
                         action='store',
                         help='set config file [not implemented]'
                     )
-    parser.add_argument('--verbose',
-                        required=False,
-                        dest='is_verbose',
-                        action='store_true',
-                        help='show verbosely output [not implemented]'
-                    )
     parser.add_argument('--version', action='version', version=version)
     args = parser.parse_args()
     
-    if args.vcf_file:
-        ans = DarnedReader(sp='human_hg19')
+    if args.vcf_file and args.species:
+        ans = DarnedReader(sp=args.species)
         vcf = VCFReader(args.vcf_file)
         bench = Benchmark(answer=ans.db, predict=vcf.db)
         print "Species:%s\tDB:%s\tVCF:%s\tPrecision:%f\tRecall:%f\tF-measure:%f\tAGs:%d\tOthers:%d\tAnsCount:%d\n" % (
