6cdfaf030111372941df5b9aae46adeacf03280d
yukke <soh.ishiguro3914@gmail.com>
2013-10-21 15:48:40 +0900

    Now working fine

--- bin/run_bench.py
+++ bin/run_bench.py
@@ -1,45 +1,43 @@
-from Ivy.benchmark import *
+from Ivy.benchmark import DarnedReader, VCFReader, Benchmark
 import argparse
 
 if __name__ == '__main__':
     version = '0.0.1'
-    desc = "Benchmarking test for detected RNA editing sites"
+    desc = "Benchmarking test for detected RNA editing sites based on HTSeq data"
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--vcf',
                         required=True,
                         dest='vcf_file',
                         action='store',
-                        type=file,
-                        help='set VCF file'
+                        help='set VCF file [required]'
                     )
     parser.add_argument('--db',
                         required=False,
                         dest='db_file',
                         action='store',
-                        type=str,
                         help='set db name'
                     )
     parser.add_argument('--config',
                         required=False,
                         dest='conf_file',
                         action='store',
-                        type=file,
-                        help='set config file'
+                        help='set config file [not implemented]'
                     )
     parser.add_argument('--verbose',
                         required=False,
                         dest='is_verbose',
                         action='store_true',
-                        help='show verbosely output'
+                        help='show verbosely output [not implemented]'
                     )
     parser.add_argument('--version', action='version', version=version)
-    
     args = parser.parse_args()
     
     if args.vcf_file:
-        v = VCFReader(args.vcf_file)
-        print v.vcf_name()
-        print v.cnt()
-        print v.editing_types()
-        print v.ag_count()
-        print v.other_mutations_count()
+        ans = DarnedReader(sp='human_hg19')
+        vcf = VCFReader(args.vcf_file)
+        bench = Benchmark(answer=ans.db, predict=vcf.db)
+        print "Species:%s\tDB:%s\tVCF:%s\tPrecision:%f\tRecall:%f\tF-measure:%f\tAGs:%d\tOthers:%d\tAnsCount:%d\n" % (
+            ans.sp(), ans.db_name(), vcf.vcf_name(),
+            bench.precision(), bench.recall(), bench.f_measure(),
+            vcf.ag_count(), vcf.other_mutations_count(), ans.count()),
+        
