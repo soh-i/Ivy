0b492cacc4b7b5601584f2a558d157b3fc95f617
yukke <soh.ishiguro3914@gmail.com>
2013-10-21 13:35:59 +0900

    change optparse to argparse

--- bin/run_bench.py
+++ bin/run_bench.py
@@ -1,22 +1,23 @@
 from Ivy.benchmark import *
-from optparse import OptionParser
+import argparse
 
 if __name__ == '__main__':
     version = '0.0.1'
-    usage = '%prog -v test.vcf'
-    parser = OptionParser(usage=usage, version=version)
-    parser.add_option(
-        '-v',
-        action='store', type='string', dest='vcf_file', help='Set vcf filename'
-    )
-    options, args = parser.parse_args()
-    if options.vcf_file:
-        v = VCFReader(options.vcf_file)
+    desc = "Benchmarking test for detected RNA editing sites"
+    parser = argparse.ArgumentParser(version=version, description=desc)
+    parser.add_argument('--vcf',
+                        required=True,
+                        dest='vcf_file',
+                        action='store',
+                        type=str,
+                        help='set VCF file'
+                    )
+    args = parser.parse_args()
+    
+    if args.vcf_file:
+        v = VCFReader(args.vcf_file)
         print v.vcf_name()
         print v.cnt()
         print v.editing_types()
         print v.ag_count()
         print v.other_mutations_count()
-    else:
-        parser.error("VCF file must be given")
-
