827f0b6f4c798722c0a9b3328aa05b21bf37eb93
yukke <soh.ishiguro3914@gmail.com>
2013-10-21 15:00:54 +0900

    Add db, config, verbose options

--- bin/run_bench.py
+++ bin/run_bench.py
@@ -4,21 +4,36 @@ import argparse
 if __name__ == '__main__':
     version = '0.0.1'
     desc = "Benchmarking test for detected RNA editing sites"
-    parser = argparse.ArgumentParser(version=version, description=desc)
-    parser.add_argument('-vcf',
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--vcf',
                         required=True,
                         dest='vcf_file',
                         action='store',
                         type=file,
                         help='set VCF file'
                     )
-    parser.add_argument('-db',
+    parser.add_argument('--db',
                         required=False,
                         dest='db_file',
                         action='store',
                         type=str,
                         help='set db name'
-    )
+                    )
+    parser.add_argument('--config',
+                        required=False,
+                        dest='conf_file',
+                        action='store',
+                        type=file,
+                        help='set config file'
+                    )
+    parser.add_argument('--verbose',
+                        required=False,
+                        dest='is_verbose',
+                        action='store_true',
+                        help='show verbosely output'
+                    )
+    parser.add_argument('--version', action='version', version=version)
+    
     args = parser.parse_args()
     
     if args.vcf_file:
