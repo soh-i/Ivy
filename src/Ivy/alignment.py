48341fef62b71f6fe1f3974ed87b1b2afb9f1cb0
yukke <soh.ishiguro3914@gmail.com>
2013-10-22 02:22:21 +0900

    Add filering class

--- src/Ivy/alignment.py
+++ src/Ivy/alignment.py
@@ -89,3 +89,22 @@ class Alignment(object):
                 self.coords.update({'end':self.end-1})
         return self.coords
 
+
+class AlignmentFiler(object):
+    def __init__(self):
+        self.edit_ratio = edit_ratio
+        self.min_mismatch = min_mismatch
+        self.coverage = coverage
+        self.is_del = False
+        self.is_unmapped = True
+        self.is_duplicate = False
+
+    def filter_with_stream(self):
+        alignment = Alignment()
+        for record in alignment:
+            if self.coverage > record['coverage'] \
+               and self.min_mismatch < record['mismatches']:
+                pass
+
+        
+
