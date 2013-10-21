a5bc46e34082ec044d7a2d25dfda2803160e42d9
yukke <soh.ishiguro3914@gmail.com>
2013-10-21 23:28:20 +0900

    Yield mismatch base only

--- bin/pileup_bam_file.py
+++ bin/pileup_bam_file.py
@@ -3,11 +3,11 @@
 from Ivy.alignment import *
 
 if __name__ == '__main__':
-    #bam_file = '../test_data/dna.bam'
-    bam_file = '/home/soh.i/db/melanogaster/Nascent-Seq/ZT18_R1/accepted_hits.bam'
-    #fa_file = '../test_data/reference.fa'
-    fa_file = '/home/soh.i/archives/nucRNA/data/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa'
-    chr_name = '2L'
+    bam_file = '../test_data/dna.bam'
+    #bam_file = '/home/soh.i/db/melanogaster/Nascent-Seq/ZT18_R1/accepted_hits.bam'
+    fa_file = '../test_data/reference.fa'
+    #fa_file = '/home/soh.i/archives/nucRNA/data/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa'
+    chr_name = 'chr21'
     start = 0
     end = 0
     alignment = Alignment(bam_file, fa_file, chrom=chr_name, start=start, end=end)
@@ -15,10 +15,14 @@ if __name__ == '__main__':
     for i in alignment.to_pileup():
         print i['chrom'], i['pos'], i['ref'],
         print i['coverage'],
-        print i['matches'],
-        print i['A'],
-        print i['T'],
-        print i['G'],
-        print i['C'],
+        print i['mismatches'],
+        print i['Af'],
+        print i['Ar'],
+        print i['Tf'],
+        print i['Tr'],
+        print i['Gf'],
+        print i['Gr'],
+        print i['Cf'],
+        print i['Gr'],
         print i['N']
         
