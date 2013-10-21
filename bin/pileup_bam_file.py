c238a8649982072be4debaab41929e7b5a3e13d3
yukke <soh.ishiguro3914@gmail.com>
2013-10-21 22:20:51 +0900

    Now it has class

--- bin/pileup_bam_file.py
+++ bin/pileup_bam_file.py
@@ -1,74 +1,85 @@
 import vcf
 import pysam
 
-def mismatch_stat(samfile, fafile, chrom=None, start=None, end=None, one_based=True):
-    coords = resolve_coords(one_based, start, end)
+class Alignment(object):
+    def __init__(self, samfile, fafile, chrom=None, start=None, end=None, one_based=None):
+        bm = pysam.Samfile(samfile, 'rb')
+        ft = pysam.Fastafile(fafile)
+        
+        self.samfile = bm
+        self.fafile = ft
+        self.chrom = chrom
+        self.start = start
+        self.end = end
+        self.one_based = one_based
+        
+    def to_pileup(self):
+        coords = self.__resolve_coords()
     
-    for col in samfile.pileup(reference=chrom, start=coords['start'], end=coords['end']):
-        chrom = samfile.getrname(col.tid)
-        pos = col.pos + 1 if one_based else col.pos
+        for col in self.samfile.pileup(reference=self.chrom, start=self.coords['start'], end=self.coords['end']):
+            chrom = self.samfile.getrname(col.tid)
+            pos = col.pos + 1 if self.one_based else col.pos
 
-        prop_read = []
-        dele_read = []
-        inser_read = []
-        
-        for r in col.pileups:
-            if r.alignment.is_proper_pair and not r.alignment.is_duplicate and not r.alignment.is_unmapped and not r.is_del:
-                prop_read.append(r)
+            prop_read = []
+            for r in col.pileups:
+                if r.alignment.is_proper_pair and not r.alignment.is_duplicate and not r.alignment.is_unmapped and not r.is_del:
+                    prop_read.append(r)
                 
-        ref = fafile.fetch(chrom, col.pos, col.pos+1).upper()
-        mismatches = [read for read in prop_read
-                      if read.alignment.seq[read.qpos] != ref]
-        matches = [read for read in prop_read
-                   if read.alignment.seq[read.qpos] == ref]
+            ref = self.fafile.fetch(self.chrom, col.pos, col.pos+1).upper()
+            mismatches = [read for read in prop_read
+                          if read.alignment.seq[read.qpos] != ref]
+            matches = [read for read in prop_read
+                       if read.alignment.seq[read.qpos] == ref]
         
-        A = [read for read in prop_read
-             if read.alignment.seq[read.qpos] == 'A']
-        C = [read for read in prop_read
-             if read.alignment.seq[read.qpos] == 'C']
-        T = [read for read in prop_read
-             if read.alignment.seq[read.qpos] == 'T']
-        G = [read for read in prop_read
-             if read.alignment.seq[read.qpos] == 'G']
-        N = [read for read in prop_read
-             if read.alignment.seq[read.qpos] == 'N']
-
-        yield {'chrom': chrom,
-               'pos': pos,
-               'ref': ref,
-               'proper_reads': len(prop_read),
-               'matches': len(matches),
-               'mismatches': len(mismatches),
-               'A': len(A),
-               'C': len(C),
-               'T': len(T),
-               'G': len(G),
-               'N': len(N)
+            A = [read for read in prop_read
+                 if read.alignment.seq[read.qpos] == 'A']
+            C = [read for read in prop_read
+                 if read.alignment.seq[read.qpos] == 'C']
+            T = [read for read in prop_read
+                 if read.alignment.seq[read.qpos] == 'T']
+            G = [read for read in prop_read
+                 if read.alignment.seq[read.qpos] == 'G']
+            N = [read for read in prop_read
+                 if read.alignment.seq[read.qpos] == 'N']
+            
+            yield {'chrom': chrom,
+                   'pos': pos,
+                   'ref': ref,
+                   'coverage': len(prop_read),
+                   'matches': len(matches),
+                   'mismatches': len(mismatches),
+                   'A': len(A),
+                   'C': len(C),
+                   'T': len(T),
+                   'G': len(G),
+                   'N': len(N)
                }
 
-def resolve_coords(one_based, start, end):
-    coords = {'start':None, 'end':None}
-    if one_based:
-        if start is not None:
-            coords.update({'start':start-1})
-        if end is not None:
-            coords.update({'end':end-1})
-    return coords
+    def __resolve_coords(self):
+        self.coords = {'start':None, 'end':None}
+        if self.one_based:
+            if self.start is not None:
+                self.coords.update({'start':self.start-1})
+            if end is not None:
+                self.coords.update({'end':self.end-1})
+        return self.coords
 
+        
 if __name__ == '__main__':
     bam_file = '../test_data/dna.bam'
-    bm = pysam.Samfile(bam_file, 'rb')
     fa_file = '../test_data/reference.fa'
-    ft = pysam.Fastafile(fa_file)
     chr_name = 'chr21'
-    start = 47743799
-    end =   47743800
-
-    for i in mismatch_stat(bm, ft, chrom=chr_name, start=start, end=end):
+    start = 47743817
+    end = 47743818
+    alignment = Alignment(bam_file, fa_file, chrom=chr_name, start=start, end=end)
+    
+    for i in alignment.to_pileup():
         print i['chrom'], i['pos'], i['ref'],
+        print i['coverage'],
         print i['matches'],
         print i['A'],
         print i['T'],
         print i['G'],
-        print i['C']
+        print i['C'],
+        print i['N']
         
