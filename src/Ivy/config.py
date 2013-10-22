4ef56fbdce504a7148242356c8097f8dcefc6fd6
yukke <soh.ishiguro3914@gmail.com>
2013-10-22 22:54:30 +0900

    Add some config

--- src/Ivy/config.py
+++ src/Ivy/config.py
@@ -1,6 +1,28 @@
+
 class AlignmentConfig(object):
     def __init__(self):
-        pass
+        self.conf = self.__set_default()
+        
+    def __set_default(self):
+        initialize = {
+            is_duplicate : False,
+            is_unmapped : False,
+            is_deletion : False,
+            is_proper_pair : True,
+            is_qcfail : False,
+            is_secondary : True,
+            mapq : 25,
+            mate_is_reverse : True,
+            mate_is_unmapped : False,
+            base_qual : 25,
+        }
 
+        
     def add_conf(self):
         pass
+
+    def read(self):
+        pass
+
+
+
