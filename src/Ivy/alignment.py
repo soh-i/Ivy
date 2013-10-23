0ecf135431f56acf1a66811464fc28371cd5d1a0
yukke <soh.ishiguro3914@gmail.com>
2013-10-24 02:33:20 +0900

    AlignmentFilter class move to new file

--- src/Ivy/alignment.py
+++ src/Ivy/alignment.py
@@ -187,18 +187,3 @@ class AlignmentStreamMerger(object):
         rna_stream = AlignmentStream(conf)
 
 
-class AlignmentFilter(object):
-    def __init__(self):
-        self.edit_ratio = edit_ratio
-        self.min_mismatch = min_mismatch
-        self.coverage = coverage
-        self.is_del = False
-        self.is_unmapped = True
-        self.is_duplicate = False
-
-    def filter_with_stream(self):
-        alignment = Alignment()
-        for record in alignment:
-            if self.coverage > record['coverage'] \
-               and self.min_mismatch < record['mismatches']:
-                pass
