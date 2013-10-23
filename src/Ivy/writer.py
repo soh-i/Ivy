02ea881d5bb5844b32fdeecb66ad783d41108dd2
yukke <soh.ishiguro3914@gmail.com>
2013-10-23 19:32:11 +0900

    some printing header methods are aggregated a public methods

--- src/Ivy/writer.py
+++ src/Ivy/writer.py
@@ -1,18 +1,24 @@
-import collections
+import sys
 from alignment import AlignmentConfig
 
 class VCFWriterException(Exception):
     pass
 
-class VCFWriter(object):
+class VCFWriteHeader(object):
     def __init__(self):
-        self.__spec = {'fileformat': 'VCFv4.1',
-                       'source': 'Ivy_v0.0.1',
-                       'reference': 'test.fasta',
-                       'species': 'Homo Sapiens',
-                       'samples': 'sample.bam',
-                   }
-        
+        self.__spec = {
+            'fileformat': 'VCFv4.1',
+            'source': 'Ivy_v0.0.1',
+            'reference': 'test.fasta',
+            'species': 'Homo Sapiens',
+            'samples': 'sample.bam',
+        }
+
+    def make_vcf_header(self):
+        sys.stdout.write(self.__spec_section())
+        sys.stdout.write(self.__params_section())
+        sys.stdout.write(self.__header_name())
+
     def merge_filtering_param(self):
         '''
         merge_filtering_param() -> dict, returns merged filtering and spec dict
@@ -21,17 +27,17 @@ class VCFWriter(object):
         self.__spec.update({k: p.conf[k] for k in p.conf})
         return self.__spec
 
-    def spec_section(self):
+    def __spec_section(self):
         '''
         spec_section() -> str, return VCF header of basic informations
         e.g. ##fileformat=VCFv4.1
         '''
         s = ''
         for k in self.__spec:
-            s += "##"+  "=".join([k, self.__spec[k]])+ "\n"
+            s += "##"+  "=".join([k, self.__spec[k]]) + "\n"
         return s
         
-    def params_section(self):
+    def __params_section(self):
         '''
         params_section()->str, returns VCF header of PARAM section
         e.g. ##PARAMS=<ID=is_qcfail,value=False>
@@ -42,7 +48,7 @@ class VCFWriter(object):
             params += "##PARAMS="+ ",".join(['<ID='+ k, 'Value='+ str(p.conf[k])])+ '>\n'
         return params
 
-    def info_section(self, id, number, value, desc):
+    def __info_section(self, id, number, value, desc):
         '''
         info_section(id, number, value, desc)->str, return INFO header
         e.g. ##INFO
@@ -55,9 +61,6 @@ class VCFWriter(object):
                             'Description='+ '"'+ desc+ '">\n'])
         return info_h
     
-    def filter(self):
-        return str('PASS')
-
-    def header(self):
+    def __header_name(self):
         return "{0}\t{1}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n".format("#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT")
 
