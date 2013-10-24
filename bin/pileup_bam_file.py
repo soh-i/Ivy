5770d16e338b6f14e1960f67da5782791e0c3a1d
yukke <soh.ishiguro3914@gmail.com>
2013-10-24 11:16:59 +0900

    Fix, prepare small and real data to test run

--- bin/pileup_bam_file.py
+++ bin/pileup_bam_file.py
@@ -2,31 +2,44 @@
 
 from Ivy.alignment import *
 from Ivy.writer import VCFWriteHeader
+import sys
+import pprint
 
 if __name__ == '__main__':
-    bam_file = '../test_data/dna.bam'
-    #bam_file = '/home/soh.i/db/melanogaster/Nascent-Seq/ZT18_R1/accepted_hits.bam'
-    fa_file = '../test_data/reference.fa'
-    #fa_file = '/home/soh.i/archives/nucRNA/data/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa'
-    chr_name = 'chr21'
-    start = 47721045
-    end = 47721088
+    SMALL = not True
+    bam_file = fa_file = chr_name = start = end = 0
+    
+    if SMALL:
+        bam_file = 'test_data/dna.bam'
+        fa_file = 'test_data/reference.fa'
+        chr_name = 'chr21'
+        start = 1
+        end = 100
+        
+    else:
+        bam_file = '/home/soh.i/db/melanogaster/Nascent-Seq/ZT18_R1/accepted_hits.bam'
+        fa_file = '/home/soh.i/archives/nucRNA/data/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa'
+        chr_name = '2L'
+        start = 1
+        end = 1000
+
+    ft = pysam.Fastafile(fa_file)
+    ref = ft.fetch(reference=chr_name, start=start, end=end)
+    
+    if ref:
+        print True
+    else:
+        print False
     
     rna_alignment = AlignmentStream(bam_file, fa_file, chrom=chr_name, start=start, end=end)
     #dna_alignment = AlignmentStream(bam_file, fa_file, chrom=chr_name, start=start, end=end)
 
     v = VCFWriteHeader()
     v.make_vcf_header()
-   
+    pp = pprint.PrettyPrinter(indent=6)
+    
     for rna in rna_alignment.pileup_stream():
-        print rna
+        #print "{0}\t{1}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n".format(rna['CHROM'], rna["POS"], rna["ID"], rna["REF"], rna["ALT"], rna["QUAL"], rna["mismatches"], rna["coverage"], rna["FORMAT"]),
+        pp.pprint(rna)
+        
 
-"""        
-        for dna in dna_alignment.pileup_stream():
-            if rna['chrom'] == dna['chrom'] and dna['pos'] == rna['pos']:
-                print dna['pos']
-
-                break;
-                
-            
-"""
