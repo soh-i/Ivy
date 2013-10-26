d6de760b937519d90f1a174e93c233e8afd88bf7
yukke <soh.ishiguro3914@gmail.com>
2013-10-26 22:47:24 +0900

    Add fetch darned (not working yet)

--- src/Ivy/utils.py
+++ src/Ivy/utils.py
@@ -1,4 +1,5 @@
 import os.path
+from urllib2 import Request, urlopen, URLError
 
 def find_app_root():
     root = os.path.dirname(__file__)
@@ -6,5 +7,29 @@ def find_app_root():
         root = os.path.abspath(os.path.join(root, os.path.pardir))
     return root
 
-if __name__ == '__main__':
-    print find_app_root()
+def fetch_darned():
+    if os.path.isfile(filename):
+        return False
+        
+    req = Request(url)
+    try:
+        response = urlopen(req)
+    except URLError, e:
+        if hasattr(e, 'reason'):
+            print 'We failed to reach a server.'
+            print 'Reason: ', e.reason
+            return False
+        elif hasattr(e, 'code'):
+            print 'The server couldn\'t fulfill the request.'
+            print 'Error code: ', e.code
+            return False
+        else:
+            root_path = os.path.dirname(filename)
+            if not os.path.isdir(root_path):
+                os.makedirs(root_path)
+                print "Made directories [%s]" % (root_path)
+                print "Dowloading [%s] from [%s] ..." % (filename, url)
+                with open(filename, "w") as fout:
+                    fout.write(response.read())
+                return True
+
