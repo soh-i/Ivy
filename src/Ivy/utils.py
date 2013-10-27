b12223a94977c17415b6799f609f0dd576c76d01
yukke <soh.ishiguro3914@gmail.com>
2013-10-27 14:47:31 +0900

    Cleasing  Darned row file to tsv

--- src/Ivy/utils.py
+++ src/Ivy/utils.py
@@ -1,4 +1,6 @@
 import os.path
+import csv
+import re
 from urllib2 import Request, urlopen, URLError
 
 def find_app_root():
@@ -10,7 +12,8 @@ def find_app_root():
 def fetch_darned():
     if os.path.isfile(filename):
         return False
-        
+
+    url = 'http://beamish.ucc.ie/data_hg19.txt'
     req = Request(url)
     try:
         response = urlopen(req)
@@ -33,3 +36,22 @@ def fetch_darned():
                     fout.write(response.read())
                 return True
 
+def to_csv(filename):
+    if not os.path.isfile(filename):
+        return  False
+    reader = csv.reader(open(filename, 'r'), delimiter="\t", quotechar="|")
+    try:
+        for row in reader:
+            source = row[8]
+            if len(source):
+                mod = source.replace(r';', ',').replace(r',', ';').replace(r'; ',';').replace(r' ','_').replace(r'_T','T')
+                print "\t".join(row[:8]),
+                print mod,
+                print "\t".join(row[9:])
+                
+    except:
+        print "Error"
+            
+
+if __name__ == '__main__':
+    to_csv('hg19.txt')
