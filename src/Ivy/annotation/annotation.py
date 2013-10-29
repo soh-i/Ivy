b2ad5c241f67983b81dcbc2d4006d10d83ebd405
yukke <soh.ishiguro3914@gmail.com>
2013-10-29 17:31:43 +0900

    Load vcf file for to comapre gtf

--- src/Ivy/annotation/annotation.py
+++ src/Ivy/annotation/annotation.py
@@ -3,5 +3,48 @@ import pysam
 class Annotation(object):
     def __init__(self):
         self.gtf = gtf
+
+    def to_array(self):
+        pass
         
+class AnnotateVCF(object):
+    def __init__(self, vcf=None, gtf=None):
+        self.__vcf = vcf
+        #self.__gtf = gtf
+
+    def vcf_to_array(self):
+        recs = []
+        with open(self.__vcf, 'r') as f:
+            for line in f:
+                if not line.startswith("#"):
+                    data = line.split("\t")
+                    chrom = data[0]
+                    pos = data[1]
+                    recs.append([chrom, pos])
+        return recs
+                    
+
+    #def gtf_to_array(self):
+    #    with open(self.__gtf, 'w') as f:
+    #        for line in f:
+    #            data = split.line("\t")
+                
+    def compare(self):
+        pass
+
+    def write_gtf(self):
+        pass
+
         
+def main():
+    a = AnnotateVCF(vcf="../../../data/test_data.vcf")
+
+    print a.vcf_to_array()[:10][:10]
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+    
