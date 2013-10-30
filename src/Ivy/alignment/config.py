980ea1e4b60414f08b4399f39e2cc40ceee4cbb9
yukke <soh.ishiguro3914@gmail.com>
2013-10-30 21:57:46 +0900

    Un-used imutalbe dictionary, uses namedtuple now.

--- src/Ivy/alignment/config.py
+++ src/Ivy/alignment/config.py
@@ -1,28 +1,47 @@
+from Ivy.utils import ImutableDict
+from collections import namedtuple
 
 class AlignmentConfig(object):
     def __init__(self):
-        self.conf = self.__set_default()
+        self.conf = self.__init_conf()
         
-    def __set_default(self):
-        initialize = {
-            is_duplicate : False,
-            is_unmapped : False,
-            is_deletion : False,
-            is_proper_pair : True,
-            is_qcfail : False,
-            is_secondary : True,
-            mapq : 25,
-            mate_is_reverse : True,
-            mate_is_unmapped : False,
-            base_qual : 25,
-        }
+    def __init_conf(self):
+        _conf = namedtuple(
+            'Conf', [
+                'is_duplicate',
+                'is_unmapped',
+                'is_deletion',
+                'is_proper_pair',
+                'is_qcfail',
+                'is_secondary',
+                'mate_is_reverse',
+                'mate_is_unmapped',
+                'mapq',
+                'base_qual',
+                'edit_ratio',
+                'edit_base_c',
+                'mutation_type_c',
+            ])
+        __c = _conf(
+            is_duplicate=False,
+            is_unmapped=False,
+            is_deletion=False,
+            is_proper_pair=True,
+            is_qcfail=False,
+            is_secondary=True,
+            mate_is_reverse=True,
+            mate_is_unmapped=False,
+            mapq=25,
+            base_qual=25,
+            edit_ratio=0.1,
+            edit_base_c=10,
+            mutation_type_c=1,
+        )
+        return __c
+
+if __name__ == '__main__':
+    cnf = AlignmentConfig()
+    print cnf.conf
+
 
         
-    def add_conf(self):
-        pass
-
-    def read(self):
-        pass
-
-
-
