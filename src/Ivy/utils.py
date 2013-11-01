e2dfdc3368d2050e88b55d9fa770796ddb191ed5
yukke <soh.ishiguro3914@gmail.com>
2013-11-01 12:25:01 +0900

    Has staticmethod

--- src/Ivy/utils.py
+++ src/Ivy/utils.py
@@ -1,20 +1,23 @@
-import os.pathn
+import os.path
 import collections
 
-def find_app_root():
-    '''
-    Absolute path to your project root from setup.py location
-    '''
-    root = os.path.dirname(__file__)
-    while not os.path.exists(os.path.join(root, 'setup.py')):
-        root = os.path.abspath(os.path.join(root, os.path.pardir))
-    return root
+class Utils(object):
+    
+    @staticmethod
+    def find_app_root():
+        '''
+        Absolute path to your project root from setup.py location
+        '''
+        root = os.path.dirname(__file__)
+        while not os.path.exists(os.path.join(root, 'setup.py')):
+            root = os.path.abspath(os.path.join(root, os.path.pardir))
+        return root
 
-def __end_url_basename(p):
-    """Returns the final component of a pathname"""
-    i = p.rfind('/') + 1
-    return p[i:]
-
+    @staticmethod
+    def __end_url_basename(p):
+        """Returns the final component of a pathname"""
+        i = p.rfind('/') + 1
+        return p[i:]
     
 class ImutableDict(collections.Mapping):
     '''
