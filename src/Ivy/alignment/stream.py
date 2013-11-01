85e38bcf9d0b327375cf664bb65d2686eb31c77e
yukke <soh.ishiguro3914@gmail.com>
2013-11-01 21:02:20 +0900

    Debugging for same things, is too hard to solved

--- src/Ivy/alignment/stream.py
+++ src/Ivy/alignment/stream.py
@@ -5,7 +5,6 @@ import re
 import pysam
 from Ivy.utils import ImutableDict
 
-
 class AlignmentConfig(object):
     def __init__(self):
         self.params = self.__set_default()
@@ -38,8 +37,7 @@ class AlignmentStream(object):
         self.samfile = __bm
         self.fafile = __ft
         self.one_based = one_based
-        
-        (self.start, self.end) = self.__resolve_coords(start, end)
+        (self.start, self.end) = self.__resolve_coords(start, end, one_based)
 
         if not chrom.startswith('chr'):
             self.chrom = 'chr' + chrom
@@ -76,21 +74,23 @@ class AlignmentStream(object):
             bam_chrom = self.samfile.getrname(col.tid)
             if self.one_based:
                 pos = col.pos + 1
-            else: pos = col.pos
+            else:
+                pos = col.pos
+            #pos = col.pos + 1 if self.one_based else col.pos
             
             ref = self.fafile.fetch(reference=bam_chrom, start=col.pos, end=col.pos+1).upper()
             reads = col.pileups
-
+            
             # Raw reads (no filterings through)
             raw_reads = [_ for _ in reads]
             raw_mismatches = [_ for _ in raw_reads if _.alignment.seq[_.qpos] != ref]
             raw_matches = [_ for _ in raw_reads if _.alignment.seq[_.qpos] == ref]
-            
+
             # Has proper_pair and without deletion
             prop_nodel_reads = [_ for _ in reads if not _.is_del and _.alignment.is_proper_pair]
             prop_nodel_mismatchs = [_ for _ in prop_nodel_reads if _.alignment.seq[_.qpos] != ref]
             prop_nodel_matches = [_ for _ in prop_nodel_reads if _.alignment.seq[_.qpos] == ref]
-
+            
             # Has NO deletion
             nodel_reads = [_ for _ in reads if not _.is_del]
             nodel_mismatches = [_ for _ in nodel_reads if _.alignment.seq[_.qpos] != ref]
@@ -102,22 +102,26 @@ class AlignmentStream(object):
             prop_matches =  [_ for _ in prop_reads if _.alignment.seq[_.qpos] == ref]
             
             # Has deletions alone
-            del_reads = [_ for _ in reads if _.is_del]
+            del_reads = [_ for _ in reads if not _.is_del]
             del_prop_reads = [_ for _ in reads if not _.is_del]
 
             # Has insertion alone
             ins_reads = [_ for _ in reads if _.is_del > 0]
             ins_prop_reads = [_ for _ in reads if _.is_del > 0]
             
-            #prop_read = []
-            #for r in col.pileups:
-            #    if r.alignment.is_proper_pair \
-            #       and not r.alignment.is_secondary \
-            #       and not r.alignment.is_qcfail \
-            #       and not r.alignment.is_duplicate \
-            #       and not r.alignment.is_unmapped \
-            #       and not r.is_del:
-            #    prop_read.append(r)
+            filt_reads = []
+            for _ in col.pileups:
+                if _.alignment.is_proper_pair \
+                   and not _.alignment.is_secondary \
+                   and not _.alignment.is_qcfail \
+                   and not _.alignment.is_duplicate \
+                   and not _.alignment.is_unmapped \
+                   and not _.is_del:
+                    filt_reads.append(_)
+                    
+            filt_mismatches = [_ for _ in filt_reads if _.alignment.seq[_.qpos] != ref]
+            filt_matches = [_ for _ in filt_reads if _.alignment.seq[_.qpos] == ref]
+
             
             if not ref:
                 raise ValueError('No seq. content within [chr:%s, start:%s, end:%s]' % \
@@ -129,20 +133,30 @@ class AlignmentStream(object):
             G = [_ for _ in prop_reads if _.alignment.seq[_.qpos] == 'G']
             N = [_ for _ in prop_reads if _.alignment.seq[_.qpos] == 'N']
             
-            G_r = [_.alignment.is_reverse for _ in G if _.alignment.is_reverse]
-            g_f = [_.alignment.is_reverse for _ in G if not _.alignment.is_reverse]
-            
-            A_r = [_.alignment.is_reverse for _ in A if _.alignment.is_reverse]
-            a_f = [_.alignment.is_reverse for _ in A if not _.alignment.is_reverse]
-            
-            T_r = [_.alignment.is_reverse for _ in T if _.alignment.is_reverse]
-            t_f = [_.alignment.is_reverse for _ in T if not _.alignment.is_reverse]
-            
-            C_r = [_.alignment.is_reverse for _ in C if _.alignment.is_reverse]
-            c_f = [_.alignment.is_reverse for _ in C if not _.alignment.is_reverse]
-                
-            N_r = [_.alignment.is_reverse for _ in N if _.alignment.is_reverse]
-            n_f = [_.alignment.is_reverse for _ in N if not _.alignment.is_reverse]
+            G_r = [_.alignment.is_reverse for _ in G
+                   if _.alignment.is_reverse]
+            g_f = [_.alignment.is_reverse for _ in G
+                   if not _.alignment.is_reverse]
+            A_r = [_.alignment.is_reverse for _ in A
+                   if _.alignment.is_reverse]
+            a_f = [_.alignment.is_reverse for _ in A
+                   if not _.alignment.is_reverse]
+            T_r = [_.alignment.is_reverse for _ in T
+                   if _.alignment.is_reverse]
+            t_f = [_.alignment.is_reverse for _ in T
+                   if not _.alignment.is_reverse]
+            C_r = [_.alignment.is_reverse for _ in C
+                   if _.alignment.is_reverse]
+            c_f = [_.alignment.is_reverse for _ in C
+                   if not _.alignment.is_reverse]
+            N_r = [_.alignment.is_reverse for _ in N
+                   if _.alignment.is_reverse]
+            n_f = [_.alignment.is_reverse for _ in N
+                   if not _.alignment.is_reverse]
+
+
+            mutation_type = {'A': len(A), 'T': len(T), 'G': len(G), 'C': len(C)}
+            print mutation_type
             
             debug = False
             if debug:
@@ -151,14 +165,25 @@ class AlignmentStream(object):
                 print '[A:%s,%s] [T:%s,%s] [G:%s,%s] [C:%s,%s]' % (A_r, a_f, T_r, t_f, G_r, g_f, C_r, c_f)
                 print 'Coverage:%d' % (coverage)
                 
+            try:
+                pass
+                #allele_freq = mismatch_c / len(raw_reads)
+                #ag_freq = (mc_G + mc_g) / (mc_G + mc_g + mc_A + mc_a)
+            except ZeroDivisionError:
+                pass
+                
             yield {
                 'CHROM': self.chrom,
                 'POS': pos,
                 'REF': ref,
-                'coverage': len(raw_reads),
+                'raw_coverage': len(raw_reads),
+                'prop_coverage': len(prop_reads),
+                'prop_nodel_coverage': len(prop_nodel_reads),
+                'nodel_coverage': len(nodel_reads),
                 'mismatches': len(raw_mismatches),
                 'matches': len(raw_matches),
                 'cov': len(raw_mismatches) + len(raw_matches),
+                'types': mutation_type,
                 'A': len(A),
                 'T': len(T),
                 'C': len(C),
@@ -166,8 +191,8 @@ class AlignmentStream(object):
                 'N': len(N),
             }
             
-    def __resolve_coords(self, start, end):
-        if self.one_based:
+    def __resolve_coords(self, start, end, is_one_based):
+        if is_one_based:
             if start is not None:
                 start = start -1
             else:
