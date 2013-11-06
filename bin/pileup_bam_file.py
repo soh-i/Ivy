9f2443e15b1e34cb8dc48f1355a079e2129a5242
yukke <soh.ishiguro3914@gmail.com>
2013-11-07 01:52:28 +0900

    Fix

--- bin/pileup_bam_file.py
+++ bin/pileup_bam_file.py
@@ -7,31 +7,25 @@ import pprint
 import csv
 
 if __name__ == '__main__':
-    SMALL = True
+    SMALL = False
+    
     if SMALL:
-        bam_file = '/Users/yukke/dev/data/testREDItools/dna.bam'
+        bam_file = '/Users/yukke/dev/data/testREDItools/rna.bam'
         fa_file = '/Users/yukke/dev/data/testREDItools/reference.fa'
         chr_name = 'chr21'
         start = 47721030
         end = 47721057
         
     else:
-        bam_file = '/home/soh.i/db/melanogaster/Nascent-Seq/ZT18_R1/accepted_hits.bam'
-        fa_file = '/home/soh.i/archives/nucRNA/data/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa'
-        chr_name = '2L'
-        start = 1
-        end = 1000
-
-    ft = pysam.Fastafile(fa_file)
-    ref = ft.fetch(reference=chr_name, start=start, end=end)
-    #print ref
-    
-
+        bam_file = '/Users/yukke/dev/data/GSM958732_hg19_wgEncodeCaltechRnaSeqHepg2R2x75Il200SplicesRep1V2.sorted.bam'
+        fa_file = '/Users/yukke/dev/data/testREDItools/reference.fa'
+        #fa_file = '/Users/yukke/dev/data/genome.fa'
+        chr_name = 'chr21'
+        start = 47721030
+        end =   48129895
+        
     rna_alignment = AlignmentStream(bam_file, fa_file, chrom=chr_name, start=start, end=end)
-    #dna_alignment = AlignmentStream(bam_file, fa_file, chrom=chr_name, start=start, end=end)
-
-    v = VCFWriteHeader()
-    #v.make_vcf_header()
+    
     pp = pprint.PrettyPrinter(indent=6)
     
     for rna in rna_alignment.pileup_stream():
@@ -39,10 +33,29 @@ if __name__ == '__main__':
 
         #print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % \
             #(rna['CHROM'], rna['POS'], rna['REF'], rna['raw_coverage'], rna['nodel_coverage'], rna['prop_coverage'], rna['prop_nodel_coverage'])
-        #pp.pprint(rna)
         print [str(_)+ ":"+ str(rna[_])for _ in rna]
         
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
