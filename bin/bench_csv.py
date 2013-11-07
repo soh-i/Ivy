20c133f4df9dbf8b7f3a5018f743bbb3b29ba572
yukke <soh.ishiguro3914@gmail.com>
2013-11-07 23:45:50 +0900

    Generate db from csv file

--- bin/bench_csv.py
+++ bin/bench_csv.py
@@ -1,16 +1,50 @@
 from Ivy.benchmark.benchmark import (
     DarnedDataGenerator,
     DarnedReader,
-    VCFReader,
     Benchmark,
+    CSVReader,
     )
 from Ivy.benchmark.plot import BenchmarkPlot
+from Ivy.version import __version__
 import Ivy.utils
 import argparse
 import os.path
 
+__program__ = 'ivy_benchmark'
+__author__ = 'Soh Ishiguro <yukke@g-language.org>'
+__license__ = ''
+__status__ = 'development'
 
 def run():
+    version = __version__
+    desc = "For CSV data"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--csv',
+                        required=True,
+                        dest='csv_file',
+                        action='store',
+                        help='set CSV file [required]'
+                    )
+    parser.add_argument('--source',
+                        required=False,
+                        dest='source',
+                        action='store',
+                        help='use specific sample/tissue/cell line (default:All)'
+                    )
+    parser.add_argument('--sp',
+                        required=True,
+                        dest='sp',
+                        action='store',
+                        help='set species and genome version (eg. human_hg19) [required]'
+                    )
+    parser.add_argument('--plot',
+                       required=False,
+                       default=False,
+                       action='store_true',
+                       help='plot benchmarking stats (default:False)',
+                   )
+    parser.add_argument('--version', action='version', version=version)
+    
     args = parser.parse_args()
     gen = DarnedDataGenerator(species=args.sp)
     
@@ -26,8 +60,8 @@ def run():
         
     if args.vcf_file and args.sp:
         ans = DarnedReader(sp=args.sp, source=args.source)
-        vcf = VCFReader(args.vcf_file)
-        bench = Benchmark(answer=ans.db, predict=vcf.db)
+        csv = CSVReader(args.csv_file)
+        bench = Benchmark(answer=ans.db, predict=csv.db)
         
         print "Species:%s,DB:%s,VCF:%s,Precision:%f,Recall:%f,F-measure:%f,AGs:%d,Others:%d,AnsCount:%d" % (
             ans.sp()[0], ans.db_name(), vcf.vcf_name(),
@@ -40,6 +74,7 @@ def run():
             bplt.plot_accuracy(lab=str(vcf.vcf_name()),
                                recall=int(bench.recall()),
                                precision=int(bench.recall()))
+            
             
 if __name__ == '__main__':
     run()
