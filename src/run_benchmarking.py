639b7b3b4396d453c5fa05b120f33b79853cdd28
yukke <soh.ishiguro3914@gmail.com>
2013-11-09 02:02:12 +0900

    Support multiple vcf files input now

--- src/run_benchmarking.py
+++ src/run_benchmarking.py
@@ -23,6 +23,7 @@ def run():
                         required=True,
                         dest='vcf_file',
                         action='store',
+                        nargs='+',
                         help='set VCF file [required]'
                     )
     parser.add_argument('--source',
@@ -50,28 +51,39 @@ def run():
     
     darned_raw_file = gen.saved_abs_path + gen.filename
     if not os.path.isfile(darned_raw_file):
-        print "fetching from darned..."
+        print "Fetching from darned..."
         gen.fetch_darned()
         
     darned_parsed_csv = gen.out_name
     if not os.path.isfile(darned_parsed_csv):
         print "parsing darned..."
         gen.darned_to_csv()
-        
+
     if args.vcf_file and args.sp:
+        print "Species,DB,VCF,Precision,Recall,F-measure,AGs,Others,AnsCount"
         ans = DarnedReader(sp=args.sp, source=args.source)
-        vcf = VCFReader(args.vcf_file)
-        bench = Benchmark(answer=ans.db, predict=vcf.db)
         
-        print "Species:%s,DB:%s,VCF:%s,Precision:%f,Recall:%f,F-measure:%f,AGs:%d,Others:%d,AnsCount:%d" % (
-            ans.sp()[0], ans.db_name(), vcf.vcf_name(),
-            bench.precision(), bench.recall(), bench.f_measure(),
-            vcf.ag_count(), vcf.other_mutations_count(), ans.size())
+        for v in args.vcf_file:
+            vcf = VCFReader(v)
+            bench = Benchmark(answer=ans.db, predict=vcf.db)
+            
+            print "%s,%s,%s,%f,%f,%f,%d,%d,%d" % (
+                ans.sp()[0],
+                ans.db_name(),
+                vcf.vcf_name(),
+                bench.precision(),
+                bench.recall(),
+                bench.f_measure(),
+                vcf.ag_count(),
+                vcf.other_mutations_count(),
+                ans.size()
+            )
 
         if args.plot:
             name = os.path.basename(args.vcf_file).split('.')[0]
             bplt = BenchmarkPlot('plot_' + name)
             bplt.plot_accuracy(lab=str(vcf.vcf_name()),
-                               recall=int(bench.recall()),
-                               precision=int(bench.recall()))
+                               recall=float(bench.recall()),
+                               precision=float(bench.precisionl()))
+            
             
