f8a0f95106cbe9049ca57d2edd7f18227115a06c
yukke <soh.ishiguro3914@gmail.com>
2013-11-10 10:10:38 +0900

    Change help messages and explain msg

--- src/run_benchmarking.py
+++ src/run_benchmarking.py
@@ -17,41 +17,47 @@ __license__ = ''
 __status__ = 'development'
 
 def run():
-    desc = "Benchmarking test for detected RNA editing sites based on HTSeq data."
+    desc = "Benchmarking test for detected RNA editing sites based on HTSeq data to evaluate detection params."
     
-    parser = argparse.ArgumentParser(description=desc)
+    parser = argparse.ArgumentParser(description=desc,
+                                     prog=__program__,
+                                     )
     group = parser.add_mutually_exclusive_group(required=True)
     
     group.add_argument('--vcf',
                        dest='vcf_file',
                        action='store',
                        nargs='+',
-                       help='set VCF file [required]'
-                   )
+                       metavar='',
+                       help='set VCF file(s)',
+                       )
     group.add_argument('--csv',
                        dest='csv_file',
                        action='store',
                        nargs='+',
-                       help='set CSV file [required]'
-                   )
+                       metavar='',
+                       help='set CSV file(s)',
+                       )
     parser.add_argument('--source',
                         required=False,
                         dest='source',
                         action='store',
-                        help='use specific sample/tissue/cell line (default:All)'
-                    )
+                        metavar='',
+                        help='use specific sample/tissue/cell line [default: All]',
+                        )
     parser.add_argument('--sp',
                         required=True,
                         dest='sp',
+                        metavar='species',
                         action='store',
-                        help='set species and genome version (eg. human_hg19) [required]'
-                    )
+                        help='set species and genome version (eg. human_hg19)',
+                        )
     parser.add_argument('--plot',
-                       required=False,
-                       default=False,
-                       action='store_true',
-                       help='plot benchmarking stats (default:False)',
-                   )
+                        required=False,
+                        default=False,
+                        action='store_true',
+                        help='plot benchmarking stats [default: Off]',
+                        )
     parser.add_argument('--version', action='version', version=__version__)
     
     args = parser.parse_args()
