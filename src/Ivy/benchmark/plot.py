98adad6de0e8c4a4c5940bc72b59d0526ab58b6d
yukke <soh.ishiguro3914@gmail.com>
2013-11-10 11:36:27 +0900

    More ggolot2

--- src/Ivy/benchmark/plot.py
+++ src/Ivy/benchmark/plot.py
@@ -2,12 +2,16 @@ import matplotlib as mlab
 mlab.use('Agg')
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+from matplotlib.ticker import (
+    MultipleLocator,
+    FormatStrFormatter,
+    )
 
 __program__ = 'ivy_benchmark'
 __author__ = 'Soh Ishiguro <yukke@g-language.org>'
 __license__ = ''
 __status__ = 'development'
-    
+
     
 class BenchmarkPlot(object):
     '''
@@ -31,23 +35,34 @@ class BenchmarkPlot(object):
         fig = plt.figure()
         
         axes = plt.subplot(111)
+    
         axes.spines['right'].set_color('none')
         axes.spines['top'].set_color('none')
         axes.xaxis.set_ticks_position('bottom')
         axes.yaxis.set_ticks_position('left')
-        axes.xaxis.grid(False)
-        axes.yaxis.grid(False)
-        axes.patch.set_facecolor('0.85')
-        axes.set_axisbelow(True)
-
-        plt.grid(True, which='minor', color="0.92", linestyle="-", linewidth=0.7)
-        plt.grid(True, which='major', color="w", linestyle="-", linewidth=1.2)
         
         plt.xlabel("Precision")
         plt.ylabel("Recall")
         plt.ylim(0.0,1.0)
         plt.xlim(0.0,1.0)
         
+        axes.patch.set_facecolor('0.89')
+        axes.set_axisbelow(True)
+        
+        axes.xaxis.set_major_locator(MultipleLocator(0.2))
+        axes.xaxis.set_minor_locator(MultipleLocator(0.1))
+        axes.yaxis.set_major_locator(MultipleLocator(0.2))
+        axes.yaxis.set_minor_locator(MultipleLocator(0.1))
+        axes.xaxis.grid(True, "minor")
+        axes.yaxis.grid(True, "minor")
+        axes.grid(b=True, which='major', color="w", linestyle="-", linewidth=1.4)
+        axes.grid(b=True, which='minor', color="0.97", linestyle="-", linewidth=0.5)
+        
+
+                                     
+        for line in axes.xaxis.get_ticklines(minor=True) + axes.yaxis.get_ticklines(minor=True):
+            line.set_markersize(0)
+
         # remove axis border line
         for child in axes.get_children():
             # TODO: do not use mlab
@@ -60,11 +75,15 @@ class BenchmarkPlot(object):
             line.set_markersize(5)
             line.set_color("gray")
             line.set_markeredgewidth(1.4)
+
+        import matplotlib.cm as cm
+        for _ in self.precision:
+            plt.plot(self.precision, self.recall,
+                     marker='o',
+                     linestyle=".", markersize=10,
+                     markeredgecolor="none",
+                     label=self.lab)
         
-        plt.plot(self.precision, self.recall,
-                 marker='o', color="red",
-                 linestyle=".", markersize=10,
-                 markeredgecolor="red", label=self.lab)
         plt.title("Benchmarking test for detection accuracy in " + self.sp)
         
         plt.legend()
@@ -72,7 +91,7 @@ class BenchmarkPlot(object):
             lg = axes.legend_
             lg.get_frame().set_linewidth(0)
             lg.get_frame().set_alpha(0.5)
-
+        
         fig.savefig(self.filename + '.pdf')
 
     def plot_editing_type(self):
