ee57f786e28d9b7c45e6941c292855efc7ea85d0
yukke <soh.ishiguro3914@gmail.com>
2013-11-12 03:20:25 +0900

    Fix #19, Do NOT use argparse. now use optparse

--- src/run_ivy.py
+++ src/run_ivy.py
@@ -1,6 +1,6 @@
 from Ivy.version import __version__
 import Ivy.utils
-import argparse
+from optparse import OptionParser, OptionGroup
 import os.path
 
 __program__ = 'ivy'
@@ -9,28 +9,73 @@ __license__ = ''
 __status__ = 'development'
 
 
+class MyParser(OptionParser):
+    def format_epilog(self, formatter):
+        return self.epilog
+        
 def run():
-    desc = "software package for identification of RNA editing sites based on massively parallel sequencing data."
-    parser = argparse.ArgumentParser(description=desc, prog=__program__)
+    desc = "software package for identification of RNA editing sites based on massively parallel sequencing data\n"
+    usage = '%prog'
+    
+    parser = OptionParser(usage)
+    input_group = OptionGroup(parser, 'Options')
+    basic_filter_group = OptionGroup(parser, 'Basic filter options')
+    stat_filter_group = OptionGroup(parser, 'Statistical filter options')
 
-    # Basic param
-    parser.add_argument('--version', action='version', version=__version__)
-    parser.add_argument('-f', '--fasta', required=True, metavar='', dest='ref', action='store', nargs=1, help='set reference genome [fasta]')
-    parser.add_argument('-r', '--rbam', required=True, metavar='', dest='r_bams', action='store', nargs='+', help='RNA-seq file(s) [bam]')
-    parser.add_argument('-d', '--dbam', required=True, metavar='', dest='d_bams', action='store', nargs='+', help='DNA-seq file(s) [bam]')
-    parser.add_argument('-l', '--region', required=False, metavar='', dest='region', action='store', nargs=1, help='Explore specify region')
-    parser.add_argument('-G', '--GTF', required=False, metavar='', dest='gtf', action='store', nargs=1, help='GTF for annotation')
-    parser.add_argument('-p', '--num_threads', required=False, metavar='', dest='n_threads', action='store', nargs=1, help='Number of threads')
-    parser.add_argument('-o', '--out', required=True, metavar='', dest='outname', action='store', nargs=1, help='Output filename')
+    # Input params
+    input_group.add_option('-f', '--fasta',
+                           dest='fasta',
+                           action='store',
+                           metavar=' ',
+                           nargs=1,
+                           help='set reference genome [fasta]'
+                           )
+    input_group.add_option('-r', '--rbam',
+                           dest='r_bams',
+                           action='store',
+                           metavar=' ',
+                           nargs='+',
+                           help='RNA-seq file(s) [bam]',
+                           )
+    input_group.add_option('-d', '--dbam',
+                           metavar=' ',
+                           dest='d_bams',
+                           action='store',
+                           nargs='+',
+                           help='DNA-seq file(s) [bam]',
+                           )
+    input_group.add_option('-R', '--region',
+                           dest='regions',
+                           action='store',
+                           metavar=' ',
+                           nargs=2,
+                           help='Explore specify region [chr:start chr:end]'
+                           )
+    input_group.add_option('-G', '--GTF',
+                           metavar=' ',
+                           dest='gtf',
+                           action='store',
+                           nargs=1,
+                           help='GTF',
+                           )
+    input_group.add_option('-p', '--num_threads',
+                           metavar=' ',
+                           dest='n_threads',
+                           action='store',
+                           nargs=1,
+                           help='Number of threads',
+                           )
+    basic_filter_group.add_option('-e', '--max_edit_ratio',
+                                  metavar=' ',
+                                  dest='edit_ratio',
+                                  action='store',
+                                  nargs=1,
+                                  help='Max edit base ratio'
+                                  )
     
-    parser.add_argument('-o', '--out', required=True, metavar='', dest='outname', action='store', nargs=1, help='Output filename')
-    parser.add_argument('-o', '--out', required=True, metavar='', dest='outname', action='store', nargs=1, help='Output filename')
-    parser.add_argument('-o', '--out', required=True, metavar='', dest='outname', action='store', nargs=1, help='Output filename')
-    
-    
-    
-    args = parser.parse_args()
-
+    parser.add_option_group(input_group)
+    parser.add_option_group(basic_filter_group)
+    (options, args) = parser.parse_args()    
 
 if __name__ == '__main__':
     run()
