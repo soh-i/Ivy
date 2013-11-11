1468b34854011ad5707ff4b6cb435e84ac350b29
yukke <soh.ishiguro3914@gmail.com>
2013-11-11 09:07:35 +0900

    Add dependencies for setup

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 
+__author__ = 'Soh Ishiguro'
+__email__ = 'yukke@g-language.org'
+__version__ = '0.0.1'
+__pkgname__ = 'Ivy'
+
 setup(
-    version='0.0.1',
-    description='',
-    author='Soh Ishiguro',
-    author_email='yukke@g-language.org',
-    url='',
-    name='Ivy',
+    version=__version__,
+    description='software package for identification of RNA editing sites based on massively parallel sequencing data',
+    author=__author__,
+    author_email=__email__
+    url='https://github.com/soh-i/Ivy',
+    name=__pkgname__,
     license='not yet',
     package=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[''],
+    install_requires=['pysam==0.7.5', 'PyVCF==0.6.4', 'matplotlib==1.3.1'],
     test_suite='Ivy.tests',
     )
-
