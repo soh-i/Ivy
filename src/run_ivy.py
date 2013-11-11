be881d6bb8f148c985457001bc109ba5581e6661
yukke <soh.ishiguro3914@gmail.com>
2013-11-12 04:04:12 +0900

    Add many options

--- src/run_ivy.py
+++ src/run_ivy.py
@@ -1,6 +1,6 @@
 from Ivy.version import __version__
 import Ivy.utils
-from optparse import OptionParser, OptionGroup
+from optparse import OptionParser, OptionGroup, HelpFormatter, IndentedHelpFormatter
 import os.path
 
 __program__ = 'ivy'
@@ -16,74 +16,88 @@ class MyParser(OptionParser):
 def run():
     desc = "software package for identification of RNA editing sites based on massively parallel sequencing data\n"
     usage = '%prog'
-    
-    parser = OptionParser(usage)
-    input_group = OptionGroup(parser, 'Options')
+
+    fmt = IndentedHelpFormatter(indent_increment=2, max_help_position=60, width=120, short_first=1)
+    parser = OptionParser(usage=usage, formatter=fmt)
     basic_filter_group = OptionGroup(parser, 'Basic filter options')
+    ext_filter_group = OptionGroup(parser, 'Extended filter options')
     stat_filter_group = OptionGroup(parser, 'Statistical filter options')
 
     # Input params
-    input_group.add_option('-f', '--fasta',
-                           dest='fasta',
-                           action='store',
-                           metavar=' ',
-                           nargs=1,
-                           help='set reference genome [fasta]'
-                           )
-    input_group.add_option('-r', '--rbam',
-                           dest='r_bams',
-                           action='store',
-                           metavar=' ',
-                           nargs='+',
-                           help='RNA-seq file(s) [bam]',
-                           )
-    input_group.add_option('-d', '--dbam',
-                           metavar=' ',
-                           dest='d_bams',
-                           action='store',
-                           nargs='+',
-                           help='DNA-seq file(s) [bam]',
-                           )
-    input_group.add_option('-R', '--region',
-                           dest='regions',
-                           action='store',
-                           metavar=' ',
-                           nargs=2,
-                           help='Explore specify region [chr:start chr:end]'
-                           )
-    input_group.add_option('-G', '--GTF',
-                           metavar=' ',
-                           dest='gtf',
-                           action='store',
-                           nargs=1,
-                           help='GTF',
-                           )
-    input_group.add_option('-p', '--num_threads',
-                           metavar=' ',
-                           dest='n_threads',
-                           action='store',
-                           nargs=1,
-                           help='Number of threads',
-                           )
+    parser.add_option('-f', '--fasta',
+                      dest='fasta',
+                      action='store',
+                      metavar='',
+                      nargs=1,
+                      help='set reference genome [fasta]'
+                      )
+    parser.add_option('-r', '--rbam',
+                      dest='r_bams',
+                      action='store',
+                      metavar='',
+                      nargs='+',
+                      help='RNA-seq file(s) [bam]',
+                      )
+    parser.add_option('-d', '--dbam',
+                      metavar='',
+                      dest='d_bams',
+                      action='store',
+                      nargs='+',
+                      help='DNA-seq file(s) [bam]',
+                      )
+    parser.add_option('-R', '--region',
+                      dest='regions',
+                      action='store',
+                      metavar='',
+                      nargs=2,
+                      help='Explore specify region [chr:start chr:end]'
+                      )
+    parser.add_option('-G', '--GTF',
+                      metavar='',
+                      dest='gtf',
+                      action='store',
+                      nargs=1,
+                      help='GTF',
+                      )
+    parser.add_option('-p', '--num_threads',
+                      metavar='',
+                      dest='n_threads',
+                      action='store',
+                      nargs=1,
+                      help='Number of threads',
+                      )
     basic_filter_group.add_option('-e', '--max_edit_ratio',
-                                  metavar=' ',
+                                  metavar='',
                                   dest='edit_ratio',
                                   action='store',
                                   nargs=1,
                                   help='Max edit base ratio'
                                   )
-
     stat_filter_group.add_option('-b', '--base_call_bias',
-                                  metavar=' ',
-                                  dest='baq_bias',
-                                  action='store',
-                                  nargs=1,
-                                  help='Consider base call bias'
-                                  )
+                                 metavar='',
+                                 dest='baq_bias',
+                                 action='store',
+                                 nargs=1,
+                                 help='Consider base call bias'
+                                 )
+    stat_filter_group.add_option('-s', '--strand_bias',
+                                 metavar='',
+                                 dest='strand_bias',
+                                 action='store',
+                                 nargs=1,
+                                 help='Consider strand bias'
+                                 )
+    ext_filter_group.add_option('-B', '--blat_collection',
+                                metavar='',
+                                dest='blat',
+                                action='store',
+                                nargs=1,
+                                help='Reduce mis-alignment with blat'
+                                )
     
-    parser.add_option_group(input_group)
     parser.add_option_group(basic_filter_group)
     parser.add_option_group(stat_filter_group)
+    parser.add_option_group(ext_filter_group)
     (options, args) = parser.parse_args()    
 
 if __name__ == '__main__':
