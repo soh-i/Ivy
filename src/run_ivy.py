70c5be6635932b6289f07aa72db44dc53e11f4d2
yukke <soh.ishiguro3914@gmail.com>
2013-11-12 19:42:51 +0900

    Fix binary

--- src/run_ivy.py
+++ src/run_ivy.py
@@ -1,6 +1,7 @@
 from Ivy.version import __version__
 from Ivy.alignment.stream import AlignmentConfig
 from Ivy.parse_opt import CommandLineParser
+import logging
 
 __program__ = 'run_ivy'
 __author__ = 'Soh Ishiguro <yukke@g-language.org>'
@@ -9,13 +10,13 @@ __status__ = 'development'
 
 class Ivy(CommandLineParser):
     def __init__(self):
+        logging.basicConfig(level=logging.ERROR, format="%(asctime)s %(message)s")
+        logging.error('Job started')
+
+    def run(self):
         CommandLineParser.__init__(self)
         self.parse()
 
-        #logging.basicConfig(level=logging.ERROR, format="%(asctime)s %(message)s")
-        #logging.error('Job started')
         #align_conf = AlignmentConfig(passed_params)
         
-if __name__ == '__main__':
-    ivy = Ivy()
     
