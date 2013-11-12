7d85b8ccfb41e5d31bbb1fff3517a38e25c6de19
yukke <soh.ishiguro3914@gmail.com>
2013-11-13 00:50:11 +0900

    Fix conflict

--- src/run_ivy.py
+++ src/run_ivy.py
@@ -1,12 +1,18 @@
 from Ivy.version import __version__
-from Ivy.alignment.stream import AlignmentConfig
+from Ivy.alignment.stream import (
+    AlignmentConfig,
+    AlignmentStream,
+    AlignmentPreparation,
+    )
 from Ivy.parse_opt import CommandLineParser
 import logging
+import pprint
 
 __program__ = 'run_ivy'
 __author__ = 'Soh Ishiguro <yukke@g-language.org>'
 __license__ = ''
 __status__ = 'development'
+
 
 class Ivy(CommandLineParser):
     def __init__(self):
@@ -15,8 +21,18 @@ class Ivy(CommandLineParser):
 
     def run(self):
         CommandLineParser.__init__(self)
-        self.parse()
+        params = self.ivy_parse_options()
 
-        #align_conf = AlignmentConfig(passed_params)
+        # just test code is here
+        params.update({'chrom': '21', 'start': 47721030, 'end': 47721057, 'one_based': True})
+        align_conf = AlignmentConfig(params)
+        print align_conf.cl_params
+        
+        stream = AlignmentStream(align_conf.cl_params)
+        pp = pprint.PrettyPrinter(indent=6)
+        for rna in stream.pileup_stream():
+            pp.pprint(rna)
+         
+
         
     
