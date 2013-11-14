9eb965762e47685bc0213a5acb9b73c463ece6be
yukke <soh.ishiguro3914@gmail.com>
2013-11-14 13:33:42 +0900

    Add info

--- src/Ivy/alignment/filter.py
+++ src/Ivy/alignment/filter.py
@@ -1,6 +1,12 @@
 from Ivy.alignment.stream import AlignmentStream
 from Ivy.annotation.writer import VCFWriteHeader
 
+__program__ = 'filter'
+__author__ = 'Soh Ishiguro <yukke@g-language.org>'
+__license__ = ''
+__status__ = 'development'
+
+
 class AlignmentReadFilter(AlignmentStream):
     def __init__(self):
         AlignmentReadFilter.__init__(self)
