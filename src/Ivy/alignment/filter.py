81c6dd5222b0df6e719d9a3128ed01aa5072e636
yukke <soh.ishiguro3914@gmail.com>
2013-11-14 12:17:13 +0900

    Has new class

--- src/Ivy/alignment/filter.py
+++ src/Ivy/alignment/filter.py
@@ -1,18 +1,17 @@
 from stream import AlignmentStream
 
-class AlignmentFilter(object):
-    def __init__(self):
-        
-        #self.edit_ratio = edit_ratio
-        #self.min_mismatch = min_mismatch
-        #self.coverage = coverage
-        #self.is_del = False
-        #self.is_unmapped = True
-        #self.is_duplicate = False
-        
-    def filter_with_stream(self):
-        alignment = Alignment()
-        for record in alignment:
-            if self.coverage > record['coverage'] \
-               and self.min_mismatch < record['mismatches']:
-                pass
+class AlignmentReadFilter(AlignmentStream):
+    pass
+
+class BasicReadFilter(object):
+    pass
+
+class StatReadFilter(object):
+    pass
+
+class ExtReadFilter(object):
+    pass
+
+    
+
+    
