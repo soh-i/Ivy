a7008c43fb9a826f40fcd1f077358900ad17081a
yukke <soh.ishiguro3914@gmail.com>
2013-11-14 12:24:07 +0900

    AlignmentPreparation moves to prepare.py

--- src/Ivy/alignment/stream.py
+++ src/Ivy/alignment/stream.py
@@ -49,58 +49,6 @@ class AlignmentConfig(object):
         pass
 
         
-class AlignmentPreparation(object):
-    def __init__(self):
-        # TODO: AlignmentStream.__init__ move into here.
-        pass
-        
-    def alignment_prepare(self):
-        raise NotImplementedError
-    
-    def __sort(self):
-        if not os.path.isfile(bamfile):
-            try:
-                pysam.sort(self.samfile, self.samfile + 'sorted')
-                sort_log = pysam.sort.getMessage()
-                return True
-            except:
-                raise RuntimeError()
-        else:
-            print "already sorted"
-            return False
-
-    def __index(self):
-        if not os.path.isfile(samfile + '.index.bam'):
-            try:
-                pysam.index(self.samfile)
-                return True
-            except:
-                raise RuntimeError()
-        else:
-            print "already indexed"
-            return False
-
-    def __faidx(self):
-        if not os.path.isfile(fafile + '.fai'):
-            try:
-                pysam.faidx(self.fafile)
-                return True
-            except:
-                raise RuntimeError()
-        else:
-            print "already exist"
-            return False 
-
-    def __merge_bams(self, bams=[]):
-        for _ in bams:
-            if not os.path.isfile(_):
-                raise RuntimeError()
-        try:
-            pysam.merge([_ for _ in bams])
-            return True
-        except:
-            raise RuntimeError()
-
 def __resolve_chrom_name(bam, fa):
     bam_obj = pysam.Samfile(bam, 'rb')
     fa_obj = pysam.Fastafile(fa)
