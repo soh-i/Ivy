25f96c6e43c7d4735ac0f8b23705dcc0bbdb20fa
yukke <soh.ishiguro3914@gmail.com>
2013-11-16 09:52:44 +0900

    Fix properly running

--- src/run_ivy.py
+++ src/run_ivy.py
@@ -1,6 +1,5 @@
 from Ivy.version import __version__
 from Ivy.alignment.stream import AlignmentStream
-from Ivy.alignment.filter import AlignmentReadFilter
 from Ivy.parse_opt import CommandLineParser
 import logging
 import pprint
@@ -16,16 +15,12 @@ class Ivy(object):
         logging.basicConfig(level=logging.ERROR, format="%(asctime)s %(message)s")
         logging.error('[Ivy] Job started')
         
-        
     def run(self):
         pp = pprint.PrettyPrinter(indent=6)
-        params = CommandLineParser()
-        stream = AlignmentStream(params.ivy_parse_options())
-        #stream = AlignmentReadFilter(params.ivy_parse_options())
-        
+        parse = CommandLineParser()
+        params = parse.ivy_parse_options()
+        stream = AlignmentStream(params)
+                
         for rna in stream.pileup_stream():
-            #pp.pprint(rna)
             if rna['mismatches'] > 10:
                 print "%s\t%s\t%s\t%s" % (rna['chrom'], rna["pos"], rna['ref'], rna['alt'])
-                #raise SystemExit()
-            
