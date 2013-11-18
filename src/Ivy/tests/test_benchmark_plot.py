6ddc39138b0498d4f48a5e4c66b7dd0706a69311
yukke <soh.ishiguro3914@gmail.com>
2013-11-18 15:12:48 +0900

    Add unittest generated by pythoscope

---
+++ src/Ivy/tests/test_benchmark_plot.py
@@ -0,0 +1,29 @@
+import unittest
+
+class TestBenchmarkPlot(unittest.TestCase):
+    def test___init__(self):
+        # benchmark_plot = BenchmarkPlot(filename, sp)
+        assert False # TODO: implement your test here
+
+    def test_plot_accuracy(self):
+        # benchmark_plot = BenchmarkPlot(filename, sp)
+        # self.assertEqual(expected, benchmark_plot.plot_accuracy(lab, recall, precision))
+        assert False # TODO: implement your test here
+
+    def test_plot_ag_enrichment(self):
+        # benchmark_plot = BenchmarkPlot(filename, sp)
+        # self.assertEqual(expected, benchmark_plot.plot_ag_enrichment())
+        assert False # TODO: implement your test here
+
+    def test_plot_all_stats(self):
+        # benchmark_plot = BenchmarkPlot(filename, sp)
+        # self.assertEqual(expected, benchmark_plot.plot_all_stats())
+        assert False # TODO: implement your test here
+
+    def test_plot_editing_type(self):
+        # benchmark_plot = BenchmarkPlot(filename, sp)
+        # self.assertEqual(expected, benchmark_plot.plot_editing_type())
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
