7ed4c8b231be61e877ae1265b74bd6dd9181581e
yukke <soh.ishiguro3914@gmail.com>
2013-11-20 00:51:12 +0900

    Add fasta_header()

--- src/Ivy/seq.py
+++ src/Ivy/seq.py
@@ -3,11 +3,32 @@ import re
 import os.path
 
 class Fasta(object):
-    def __init__(self, filename):
-        if os.path.isfile(filename):
-            self.filename = filename
+    def __init__(self, fa=None):
+        if os.path.isfile(fa):
+            self.filename = fa
         else:
-            raise RuntimeError("{fa:s} isnot found".format(fa=fastafile))
-
+            raise RuntimeError("{fa:s} is not found".format(fa=fastafile))
     
+    def fasta_header(self):
+        flag = False
+        count = 0
+        header = []
+        with open(self.filename, 'r') as f:
+            for line in f:
+                if line.startswith('>'):
+                    header.append(line.rstrip())
+                    count += 1
+                    flag = True
+        return header
+
+    def splitheader(self, num):
+        raise NotImplementedError()
+
+    def get_fasta_length(self):
+        raise NotImplementedError()
         
+
+if __name__ == '__main__':
+    fa = Fasta(fa="seq.fa")
+    print fa.filename
+    print fa.fasta_header()
