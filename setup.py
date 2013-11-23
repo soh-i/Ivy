439d4186704efbe856cd344d0024aee014fe9475
yukke <soh.ishiguro3914@gmail.com>
2013-11-23 22:05:11 +0900

    Works only __main__

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ __email__ = 'yukke@g-language.org'
 __version__ = '0.0.1'
 __pkgname__ = 'Ivy'
 
-setup(
-    version=__version__,
-    description='software package for identification of RNA editing sites based on massively parallel sequencing data',
-    author=__author__,
-    author_email=__email__
-    url='https://github.com/soh-i/Ivy',
-    name=__pkgname__,
-    license='not yet',
-    package=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=['pysam==0.7.5', 'PyVCF==0.6.4', 'matplotlib==1.3.1'],
-    test_suite='Ivy.tests',
-    )
+if __name__ == '__main__':
+    setup(
+        version=__version__,
+        description='software package for identification of RNA editing sites based on massively parallel sequencing data',
+        author=__author__,
+        author_email=__email__
+        url='https://github.com/soh-i/Ivy',
+        name=__pkgname__,
+        license='not yet',
+        package=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=['pysam==0.7.5', 'PyVCF==0.6.4', 'matplotlib==1.3.1'],
+        test_suite='Ivy.tests',
+        )
