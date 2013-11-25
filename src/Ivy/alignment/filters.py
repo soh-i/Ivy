d1131129437aeb9dc67a6b87812d20ba03325f31
yukke <soh.ishiguro3914@gmail.com>
2013-11-26 04:10:02 +0900

    Change argument, used alignment object

--- src/Ivy/alignment/filters.py
+++ src/Ivy/alignment/filters.py
@@ -6,11 +6,13 @@ __author__ = 'Soh Ishiguro <yukke@g-language.org>'
 __license__ = ''
 __status__ = 'development'
 
-
-def strand_bias_filter(mr, mf, mir, mif):
-    p = fisher.pvalue(mr, mf, mir, mif)
+def strand_bias_filter(match, mismatch):
+    mis_r = [_.alignment.seq[_.qpos] for _ in mismatch if _.alignment.is_reverse]
+    mis_f = [_.alignment.seq[_.qpos] for _ in mismatch if not _.alignment.is_reverse]
+    ma_r =  [_.alignment.seq[_.qpos] for _ in match if _.alignment.is_reverse]
+    ma_f =  [_.alignment.seq[_.qpos] for _ in match if not _.alignment.is_reverse]
+    p = fisher.pvalue(len(ma_r), len(ma_f), len(mis_r), len(mis_f))
     return p
-    
         
         
 
