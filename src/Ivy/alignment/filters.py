fcbeafcc1cae6302eb770c469c2d998d21270548
yukke <soh.ishiguro3914@gmail.com>
2013-11-26 04:01:25 +0900

    Implements strnad_bias_filter

--- src/Ivy/alignment/filters.py
+++ src/Ivy/alignment/filters.py
@@ -1,5 +1,5 @@
-from Ivy.alignment.stream import AlignmentStream
-from Ivy.annotation.writer import VCFWriteHeader
+import sys
+import fisher
 
 __program__ = 'filter'
 __author__ = 'Soh Ishiguro <yukke@g-language.org>'
@@ -7,31 +7,12 @@ __license__ = ''
 __status__ = 'development'
 
 
-class AlignmentReadFilter(AlignmentStream):
-    def __init__(self):
-        AlignmentStream.__init__(self, params)
-        self.pileup_stream()
-
-
-class BasicReadFilter(object):
-    def __init__(self):
-        pass
-
-class StatReadFilter(object):
-    def __init__(self):
-        pass
-
-class ExtReadFilter(object):
-    def __init__(self):
-        pass
-
-
-if __name__ == '__main__':
-    ar_filter = AlignmentReadFilter()
-
-
-
-
+def strand_bias_filter(mr, mf, mir, mif):
+    p = fisher.pvalue(mr, mf, mir, mif)
+    return p
+    
+        
+        
 
 
 
