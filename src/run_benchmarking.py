cf4f8ea5a843fc53e53b1733a026469d37149124
yukke <soh.ishiguro3914@gmail.com>
2013-11-27 15:01:17 +0900

    Refactor some modules

--- src/run_benchmarking.py
+++ src/run_benchmarking.py
@@ -2,6 +2,7 @@ import os.path
 import sys
 from Ivy.commandline.parse_benchmarking_opts import parse_bench_opts
 from Ivy.benchmark.plot import BenchmarkPlot
+from Ivy.benchmark.benchmark import *
 import Ivy.utils
 
 __program__ = 'ivy_benchmark'
@@ -11,115 +12,32 @@ __status__ = 'development'
 
 def run():
     args = parse_bench_opts()
-    db = _data_prepare(parse_opts)
-    
+    _prepare_required_data(args.sp)
+
     if args.vcf_file and args.sp:
-        content = _use_vcf()
-        if args.out:
-            _write_result(content)
-        else:
-            sys.stdout.write(content)
+        result = _call_bench(args.vcf_file, sp=args.sp, source=args.source)
         
-    elif args.vcf_file and args.sp:
-        _use_csv()
-        
-        
-def _write_result():
-    # save to file
+    header = "Species,Source,DB,VCF,Precision,Recall,F-measure,AGs,Others,AnsCount\n"
     if args.out:
-        _ = open(args.out, 'a')
-        _.write(content)
-        _.close()
-    # print stdout
-    else:
-        sys.stdout.write(content)
-
-def _use_vcf():
-    if args.vcf_file and args.sp:
-        if args.out:
-            _ = open(args.out, 'w')
-            _.write("Species,Source,DB,VCF,Precision,Recall,F-measure,AGs,Others,AnsCount\n")
-        else:
-            sys.stdout.write("Species,Source,DB,VCF,Precision,Recall,F-measure,AGs,Others,AnsCount\n")
+        _write_result(args.out, header, result)
+    elif not args.out:
+        sys.stdout.write(header)
+        sys.stdout.write(result)
         
-        ans = DarnedReader(sp=args.sp, source=args.source)
-        precision = []
-        recall = []
-        f_measure = []
-        content = str()
-        for v in args.vcf_file:
-            vcf = VCFReader(v)
-
-            try:
-                bench = Benchmark(answer=ans.db, predict=vcf.db)
-            except BenchmarkIOException as e:
-                raise SystemExit('[{0}]: {1}'.format(e.__class__.__name__, e))
-                
-            p = bench.precision()
-            r = bench.recall()
-            f = bench.f_measure()
-
-            content += (
-                '{species:s},{source:s},{db_name:s},{vcf_file:s},{precision:f},{recall:f},{f_measure:f},{ag_count:d},{other_count:d},{ans_size:d}\n'
-                .format(
-                    species=ans.sp()[0],
-                    source=args.source,
-                    db_name=ans.db_name(),
-                    vcf_file=vcf.vcf_name(),
-                    precision=p,
-                    recall=r,
-                    f_measure=f,
-                    ag_count=vcf.ag_count(),
-                    other_count=vcf.other_mutations_count(),
-                    ans_size=ans.size()))
+    if args.plot:
+        _plot(precisions, recalls, args.csv_file)
     
-def _use_csv():
-    print "Species,Source,DB,CSV,Precision,Recall,F-measure,PredCount,AnsCount"
-
-        try:
-            ans = DarnedReader(sp=args.sp, source=args.source)
-        except TypeError as e:
-            raise SystemExit('[{cls}]: {err}'.format(cls=e.__class__.__name__, err=e))
-        except BenchmarkIOException as e:
-            raise SystemExit('[{cls}]: {err}'.format(cls=e.__class__.__name__, err=e))
-            
-        precisions = []
-        recalls = []
-        f_measures = []
-        
-        for c in args.csv_file:
-            csv = __CSVReader(c)
-            bench = Benchmark(answer=ans.db, predict=csv.db)
-            p = bench.precision()
-            r = bench.recall()
-            f = bench.f_measure()
-            
-            print "%s,%s,%s,%s,%f,%f,%f,%d,%d" % (
-                ans.sp()[0],
-                args.source,
-                ans.db_name(),
-                csv.name(),
-                p, r, f,
-                csv.size(),
-                ans.size())
-
-            precisions.append(float(p))
-            recalls.append(float(r))
-            f_measures.append(float(f))
-    pass
-    
-def _data_prepare(args):
+def _prepare_required_data(species):
     try:
-        gen = DarnedDataGenerator(species=args.sp)
+        gen = DarnedDataGenerator(species)
     except DarnedDataGeneratorValueError as e:
         raise SystemExit('[{cls}]: given \'{sp}\' is not valid name, {sps} is only valid name'.format(
             cls=e.__class__.__name__, sp=e.sp, sps=e.sps))
         
-    # check darned raw file
     darned_raw_file = gen.saved_abs_path + gen.filename
     if not os.path.isfile(darned_raw_file):
         sys.stderr.write("No {f:s} file\n".format(f=darned_raw_file))
-        sys.stderr.write("Fetching {sp:s} from Darned DB...\n".format(sp=args.sp))
+        sys.stderr.write("Fetching {sp:s} from Darned DB...\n".format(sp=species))
         gen.fetch_darned()
         
     darned_parsed_csv = gen.out_name
@@ -130,36 +48,25 @@ def _data_prepare(args):
         except DarnedDataGeneratorParseError as e:
             raise SystemExit('[{cls}]: {e}'.format(cls=e.__class__.__name__, e=e))
 
-    # use VCF files
-    if args.vcf_file and args.sp:
-        if args.out:
-            _ = open(args.out, 'w')
-            _.write("Species,Source,DB,VCF,Precision,Recall,F-measure,AGs,Others,AnsCount\n")
-        else:
-            sys.stdout.write("Species,Source,DB,VCF,Precision,Recall,F-measure,AGs,Others,AnsCount\n")
-        
-        ans = DarnedReader(sp=args.sp, source=args.source)
-        precision = []
-        recall = []
-        f_measure = []
-        content = str()
-        for v in args.vcf_file:
-            vcf = VCFReader(v)
-
-            try:
-                bench = Benchmark(answer=ans.db, predict=vcf.db)
-            except BenchmarkIOException as e:
-                raise SystemExit('[{0}]: {1}'.format(e.__class__.__name__, e))
-                
-            p = bench.precision()
-            r = bench.recall()
-            f = bench.f_measure()
-
-            content += (
+def _call_bench(vcf_files, sp=None, source=None):
+    precision, recall, f_measure = [0, 0, 0]
+    content = str()
+    ans = DarnedReader(sp=sp, source=source)
+    for v in vcf_files:
+        vcf = VCFReader(v)
+        try:
+            bench = Benchmark(answer=ans.db, predict=vcf.db)
+        except BenchmarkIOException as e:
+            raise SystemExit('[{0}]: {1}'.format(e.__class__.__name__, e))
+            
+        p = bench.precision()
+        r = bench.recall()
+        f = bench.f_measure()
+        content += (
                 '{species:s},{source:s},{db_name:s},{vcf_file:s},{precision:f},{recall:f},{f_measure:f},{ag_count:d},{other_count:d},{ans_size:d}\n'
                 .format(
                     species=ans.sp()[0],
-                    source=args.source,
+                    source=source,
                     db_name=ans.db_name(),
                     vcf_file=vcf.vcf_name(),
                     precision=p,
@@ -168,59 +75,47 @@ def _data_prepare(args):
                     ag_count=vcf.ag_count(),
                     other_count=vcf.other_mutations_count(),
                     ans_size=ans.size()))
-            
-        # save to file
-        if args.out:
-            _ = open(args.out, 'a')
-            _.write(content)
-            _.close()
-        # print stdout
-        else:
-            sys.stdout.write(content)
-            
-        # Plot
-        if args.plot:
-            names = [os.path.basename(_).split('.')[0] for _ in args.vcf_file]
-            bplt = BenchmarkPlot('plot_' + ','.join(names))
-            bplt.plot_accuracy(lab=args.vcf_file, recall=r, precision=p)
-            
-    # use CSV files, this is debug mode
-    elif args.csv_file and args.sp:
-        print "Species,Source,DB,CSV,Precision,Recall,F-measure,PredCount,AnsCount"
-
-        try:
-            ans = DarnedReader(sp=args.sp, source=args.source)
-        except TypeError as e:
-            raise SystemExit('[{cls}]: {err}'.format(cls=e.__class__.__name__, err=e))
-        except BenchmarkIOException as e:
-            raise SystemExit('[{cls}]: {err}'.format(cls=e.__class__.__name__, err=e))
-            
-        precisions = []
-        recalls = []
-        f_measures = []
+        return content
         
-        for c in args.csv_file:
-            csv = __CSVReader(c)
-            bench = Benchmark(answer=ans.db, predict=csv.db)
-            p = bench.precision()
-            r = bench.recall()
-            f = bench.f_measure()
-            
-            print "%s,%s,%s,%s,%f,%f,%f,%d,%d" % (
-                ans.sp()[0],
-                args.source,
-                ans.db_name(),
-                csv.name(),
-                p, r, f,
-                csv.size(),
-                ans.size())
+def _write_result(filename, header, content):
+    f = open(filename, 'w')
+    f.write(header)
+    f.write(content)
+    f.close()
+    
+def _use_csv():
+    print "Species,Source,DB,CSV,Precision,Recall,F-measure,PredCount,AnsCount"
 
-            precisions.append(float(p))
-            recalls.append(float(r))
-            f_measures.append(float(f))
+    try:
+        ans = DarnedReader(sp=args.sp, source=args.source)
+    except TypeError as e:
+        raise SystemExit('[{cls}]: {err}'.format(cls=e.__class__.__name__, err=e))
+    except BenchmarkIOException as e:
+        raise SystemExit('[{cls}]: {err}'.format(cls=e.__class__.__name__, err=e))
+            
+    precisions = []
+    recalls = []
+    f_measures = []
         
-        if args.plot:
-            __plot(precisions, recalls, args.csv_file)
+    for c in args.csv_file:
+        csv = __CSVReader(c)
+        bench = Benchmark(answer=ans.db, predict=csv.db)
+        p = bench.precision()
+        r = bench.recall()
+        f = bench.f_measure()
+            
+        print "%s,%s,%s,%s,%f,%f,%f,%d,%d" % (
+            ans.sp()[0],
+            args.source,
+            ans.db_name(),
+            csv.name(),
+            p, r, f,
+            csv.size(),
+            ans.size())
+
+        precisions.append(float(p))
+        recalls.append(float(r))
+        f_measures.append(float(f))
 
 def __plot(p, r, labs):
     if isinstance(p, list) and isinstance(r, list) and isinstance(labs, list):
