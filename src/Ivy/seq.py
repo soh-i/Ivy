ea9574405ea30edf91a0632f494d997a6859e08f
yukke <soh.ishiguro3914@gmail.com>
2013-11-28 18:55:19 +0900

    Implement chr_size()

--- src/Ivy/seq.py
+++ src/Ivy/seq.py
@@ -35,6 +35,9 @@ class Fasta(object):
                 elif not flag:
                     continue
         return None
+
+    def chr_size(self):
+        return len(self.fasta_header())
                 
     def split_by_length(self, length):
         for i in range(1, num):
@@ -54,9 +57,12 @@ def automatically(cpus):
 
 if __name__ == '__main__':
     fasta = Fasta(fa="/Users/yukke/dev/data/genome.fa")
-    #print fasta.fasta_header()
-    #print fasta.split_by_chr()
-
+    print fasta.fasta_header()
+    #print fasta.split_by_chr)
+    human_chr = ['chrM', 'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6',
+                 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13',
+                 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20',
+                 'chr21', 'chr22', 'chrX', 'chrY']
     
     print automatically(24)
 
