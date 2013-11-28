9f0d1728e694b96bf11990c4233754ec8e5f69e0
yukke <soh.ishiguro3914@gmail.com>
2013-11-28 19:20:08 +0900

    Now writing ....

--- src/Ivy/seq.py
+++ src/Ivy/seq.py
@@ -38,7 +38,7 @@ class Fasta(object):
 
     def chr_size(self):
         return len(self.fasta_header())
-                
+    
     def split_by_length(self, length):
         for i in range(1, num):
             f = open(str(i)+'_'+str(self.filename), 'w')
@@ -57,13 +57,32 @@ def automatically(cpus):
 
 if __name__ == '__main__':
     fasta = Fasta(fa="/Users/yukke/dev/data/genome.fa")
-    print fasta.fasta_header()
+    #print fasta.fasta_header()
     #print fasta.split_by_chr)
+    
     human_chr = ['chrM', 'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6',
                  'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13',
                  'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20',
                  'chr21', 'chr22', 'chrX', 'chrY']
     
-    print automatically(24)
+    num_threads = 2
+    chr_size = len(human_chr)
+    div, mod = divmod(chr_size, num_threads)
+    result = []
+    
+    start = 0
+    end = div
+    for i in range(0, chr_size):
+        if num_threads > step:
+            step += num_threads
+            print human_chr[start:end]
+            end += div
+            start +=1
+        
+    
+    #print automatically(24)
+
+
+
 
     
