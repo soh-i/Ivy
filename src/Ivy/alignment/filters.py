4a408f4e8a80e57997ef9c80e629b6c9e8c9f07f
yukke <soh.ishiguro3914@gmail.com>
2013-12-26 02:30:40 +0900

    Implements positional_bias_filter

--- src/Ivy/alignment/filters.py
+++ src/Ivy/alignment/filters.py
@@ -1,5 +1,6 @@
 import sys
 import fisher
+from scipy import stats, mean
 
 __program__ = 'filter'
 __author__ = 'Soh Ishiguro <yukke@g-language.org>'
@@ -13,9 +14,13 @@ def strand_bias_filter(match, mismatch):
     ma_f =  [_.alignment.seq[_.qpos] for _ in match if not _.alignment.is_reverse]
     p = fisher.pvalue(len(ma_r), len(ma_f), len(mis_r), len(mis_f))
     return p.left_tail
-        
-def positional_bias_filter(match, mismatch):
-    pass
+
+def positional_bias_filter(m=None, mis=None):
+    mismatch_pos = [_.alignment.qlen for _ in mis]
+    match_pos = [_.alignment.qlen for _ in m]
+    p = stats.ttest_ind(match_pos, mismatch_pos)
+    #return (mean(match_pos), mean(mismatch_pos), stat[1])
+    return p
     
 
 
