8b9850482d4998d60b78c746d3c64670ffcaccf8
yukke <soh.ishiguro3914@gmail.com>
2013-12-26 18:32:40 +0900

    Support RNA and DNA-seq data

--- src/Ivy/run_ivy.py
+++ src/Ivy/run_ivy.py
@@ -1,9 +1,8 @@
 from Ivy.version import __version__
-from Ivy.utils import die
-from Ivy.alignment.stream import AlignmentStream, RNASeqAlignmentStream, DNASeqAlignmentStream
+from Ivy.alignment.stream import RNASeqAlignmentStream, DNASeqAlignmentStream
 from Ivy.commandline.parse_ivy_opts import CommandLineParser
 from Ivy.annotation.writer import VCFWriteHeader
-from  pprint import pprint as p
+from pprint import pprint as p
 import logging
 
 __program__ = 'run_ivy'
@@ -18,34 +17,32 @@ def run():
     parse = CommandLineParser()
     params = parse.ivy_parse_options()
     vcf = VCFWriteHeader(params)
-    #stream = AlignmentStream(params)
-    if params.r_bams:
-        logger.debug("Loading RNA-seq bam file '{0}'".format(params.r_bams))
-        rna_stream = RNASeqAlignmentStream(params)
-    if params.d_bams:
-        logger.debug("Loading DNA-seq bam file '{0}'".format(params.d_bams))
-        dna_stream = DNASeqAlignmentStream(params)
-        
     vcf.make_vcf_header()
     
+    if params.r_bams:
+        logger.debug("Loading RNA-seq bam file '{0}'".format(params.r_bams))
+        rna_pileup_alignment = RNASeqAlignmentStream(params)
+        for rna in rna_pileup_alignment.filter_stream():
+            pprint(rna)
+            #print '{chrom}\t{pos:d}\t{ref:s}\t{alt:s}\tDP={coverage:d};DP4={dp4:s};MIS_RATIO={mismatch_ratio:0.4f}'.format(
+            #chrom=rna['chrom'],
+            #pos=rna['pos'],
+            #ref=rna['ref'],
+            #alt=rna['alt'],
+            #mismatch_ratio=rna['mismatch_ratio'],
+            #coverage=rna['coverage'],
+            #dp4=",".join([str(_) for _ in rna['dp4']]))
+            
+    if params.d_bams:
+        logger.debug("Loading DNA-seq bam file '{0}'".format(params.d_bams))
+        dna_pileup_alignment = DNASeqAlignmentStream(params)
+        for dna in dna_pileup_alignment.filter_stream():
+            pprint(dna)
+
     #with open(params.outname, 'w') as f:
     #f.write()
-            
-    for rna in rna_stream.pileup_stream():
-        pprint(rna)
-        #print '{chrom}\t{pos:d}\t{ref:s}\t{alt:s}\tDP={coverage:d};DP4={dp4:s};MIS_RATIO={mismatch_ratio:0.4f}'.format(
-        #    chrom=rna['chrom'],
-        #    pos=rna['pos'],
-        #    ref=rna['ref'],
-        #    alt=rna['alt'],
-        #    mismatch_ratio=rna['mismatch_ratio'],
-        #    coverage=rna['coverage'],
-        #    dp4=",".join([str(_) for _ in rna['dp4']]))
-        
-    for dna in dna_stream.pileup_stream():
-        pprint(dna)
 
-
+    
 def pprint(data, *args, **kwargs):
     '''
     Simple pretty print yielded pileuped data
