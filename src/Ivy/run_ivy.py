efab501f17ffd2d7ba2f940cf2d21e67edbe5f78
yukke <soh.ishiguro3914@gmail.com>
2013-12-29 11:49:51 +0900

    Modify thread processing and create Printer class

--- src/Ivy/run_ivy.py
+++ src/Ivy/run_ivy.py
@@ -15,7 +15,7 @@ __license__ = ''
 __status__ = 'development'
 __version__ = __version__
 
-def run():
+def single_run():
     logger = logging.getLogger(__name__)    
     parse = CommandLineParser()
     params = parse.ivy_parse_options()
@@ -42,36 +42,44 @@ def run():
     #with open(params.outname, 'w') as f:
     #f.write()
 
+def list_fasta_files(path, suffix):
+    '''
+    Args:
+     path(str): path to directory which contains fasta files
+     suffix(str): suffix in fasta files, default sets '.fa'
+    Return:
+     fasta files(list) in given path
+    '''
+    if suffix:
+        # uses custamized suffix of fasta
+        return  [_ for _ in os.listdir(path) if _.endswith(ext)]
+    else:
+        # default
+        return  [_ for _ in os.listdir(path) if _.endswith('.fa')]
+
 def __thread_ivy(seqs):
     parse = CommandLineParser()
     params = parse.ivy_parse_options()
     vcf = VCFWriteHeader(params)
-    seqs = fasta_list("block_fasta")
     if params.r_bams: 
         for seq in seqs:
             print seq
             # Update reference genome to splited fasta
-            params.fasta = "block_fasta/"+seq
-            pileup_iter = RNASeqAlignmentStream(params)
-            for pileup in pileup_iter.filter_stream():
-                print to_tab(pileup)
+            #  pileup_iter = RNASeqAlignmentStream(params)
+            #  or pileup in pileup_iter.filter_stream():
+            #     print to_tab(pileup)
                 
-def fasta_list(path):
-    return  [_ for _ in os.listdir(path) if _.endswith('.fa')]
-    
 def __worker(cpus=1, seqs=None):
     if cpus <= 1 and len(seqs) <= 1:
         raise RuntimeError()
     p = Pool(processes=cpus)
-    seq = p.map(__thread_ivy, seqs)
+    seq = p.map(__thread_ivy, [seqs])
     return seq
 
 def thread_run():
     parse = CommandLineParser()
     params = parse.ivy_parse_options()
     path = './block_fasta'
-    fasta_file = params.fasta
-    cpus = params.n_threads
     
     # create tmp directory
     if not os.path.isdir(path):
@@ -79,45 +87,51 @@ def thread_run():
     fasta_files = [_ for _ in os.listdir(path) if _.endswith('.fa')]
     
     # generate worker
-    if len(fasta_files) != cpus:
+    if len(fasta_files) != params.n_threads:
         print "Remove old splited fasta, and generate new..."
         shutil.rmtree(path)
-        fa = Fasta(fa=fasta_file)
-        fa.split_by_blocks(n=cpus)
-        __worker(cpus=cpus, seqs=fasta_files)
+        
+        # split fasta by number of threads
+        fa = Fasta(fa=params.fasta)
+        fa.split_by_blocks(n=params.n_threads)
+        # call threads
+        __worker(cpus=params.n_threads, seqs=fasta_files)
         
     elif len(fasta_files):
         print "Used existing splited fasta..."
-        __worker(cpus=cpus, seqs=fasta_files)
-        
-def pprint(data, *args, **kwargs):
-    '''
-    Simple pretty print yielded pileuped data
-    '''
-    print '{'
-    for key in data:
-        print '  {key}({types}): {val}'.format(
-            key=key, val=data[key], types=type(data[key]).__name__)
-    print '}'
+        __worker(cpus=params.n_threads, seqs=fasta_files)
 
-def to_tsv(data, *args, **kwargs):
-    entory = ''
-    for key in data:
-        if isinstance(data[key], str):
-            entory += data[key]+"\t"
-        elif isinstance(data[key], int) or isinstance(data[key], float):
-            entory += str(data[key])+"\t"
-        elif isinstance(data[key], tuple):
-            entory += ','.join([str(_) for _ in data[key]]) + "\t"
-    return entory
-    
+class Printer(object):
+    @staticmethod
+    def pprint(data, *args, **kwargs):
+        '''
+        Simple pretty print yielded pileuped data
+        '''
+        print '{'
+        for key in data:
+            print '  {key}({types}): {val}'.format(
+                key=key, val=data[key], types=type(data[key]).__name__)
+        print '}'
+
+    @staticmethod
+    def to_tsv(data, *args, **kwargs):
+        entory = ''
+        for key in data:
+            if isinstance(data[key], str):
+                entory += data[key]+"\t"
+            elif isinstance(data[key], int) or isinstance(data[key], float):
+                entory += str(data[key])+"\t"
+            elif isinstance(data[key], tuple):
+                entory += ','.join([str(_) for _ in data[key]]) + "\t"
+        return entory
+         
+    @staticmethod
+    def to_tab(data, *args, **kwargs):
+        return '{chrom:}\t{pos:}\t{ref:}\t{alt:}\t{dp4:}'.format(
+            chrom=data.get('chrom'),
+            pos=data.get('pos'),
+            ref=data.get('ref'),
+            alt=data.get('alt'),
+            dp4=",".join([str(_) for _ in data.get('dp4')]))
             
-def to_tab(data, *args, **kwargs):
-    return '{chrom:}\t{pos:}\t{ref:}\t{alt:}\t{dp4:}'.format(
-        chrom=data.get('chrom'),
-        pos=data.get('pos'),
-        ref=data.get('ref'),
-        alt=data.get('alt'),
-        dp4=",".join([str(_) for _ in data.get('dp4')]))
-        
-    
+         
