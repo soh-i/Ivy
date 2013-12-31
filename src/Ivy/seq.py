9bee5049b43ad89974b21513f04adbb133cd00ae
yukke <soh.ishiguro3914@gmail.com>
2013-12-31 17:28:03 +0900

    Returns interval time

--- src/Ivy/seq.py
+++ src/Ivy/seq.py
@@ -7,21 +7,18 @@ import multiprocessing
 import pysam
 import pprint
 import time
+from Ivy.utils import IvyLogger
 
 class Timer(object):
-    def __init__(self, verbose=True):
-        self.verbose = verbose
-        
     def __enter__(self):
         self.start = time.time()
-
+        return self
+        
     def __exit__(self, *args):
         self.end = time.time()
-        self.secs = self.end - self.start
-        if self.verbose:
-            print '=> Elapsed time: {:f} (sec)'.format(self.secs)
+        self.interval = self.end - self.start
 
-            
+        
 class Fasta(object):
     def __init__(self, fa=''):
         if os.path.isfile(fa):
