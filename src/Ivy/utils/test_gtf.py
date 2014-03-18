519ff9be9be49770991d2ce76d483b880311ac8e
soh-i <soh.ishiguro3914@gmail.com>
2014-03-18 13:56:44 +0900

    create test suites

---
+++ src/Ivy/utils/test_gtf.py
@@ -0,0 +1,12 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+class TestGTF(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
