7acee1503951cc74ce3c7be34007f92403e2f554
soh-i <soh.ishiguro3914@gmail.com>
2014-03-22 16:24:09 +0900

    Assign to settings.py

--- src/Ivy/commandline2/load_settings.py
+++ src/Ivy/commandline2/load_settings.py
@@ -1,33 +1,18 @@
 import os.path
-import ConfigParser
-from Ivy.base.Utils import find_app_root
+from Ivy.base import Utils
+from Ivy.commandline2.settings import IVY_SETTINGS, EDIT_BENCH_SETTINGS
 
 class Setting(object):
     def __init__(self):
-        APP_ROOT = Utils.find_app_root()
-        FILE = 'conf.yml'
-        self.path_to_conf = os.path.join(APP_ROOT, FILE)
-        
-    def load(self):
-        '''
-        Return:
-         ConfigParser object
-        '''
-        
-        if os.path.isfile(self.path_to_conf):
-            config = ConfigParser()
-            self.config = config.read(self.path_to_conf)
-        else:
-            raise ValueError("{0} is not found".format(self.path_to_conf))
-    
-    def get_path(self):
-        return self.path_to_conf
-
-    def show_all(self):
-        #self.load()
         pass
-
+        
+    def load(self, cls):
+        if cls == 'IVY_SETTINGS':
+            return IVY_SETTINGS
+        elif cls == 'EDIT_BENCH_SETTINGS':
+            return EDIT_BENCH_SETTINGS
+        
 if __name__ == '__main__':
     setting = Setting()
-    setting.load()
+    print setting.load('EDIT_BENCH_SETTINGS')
     
