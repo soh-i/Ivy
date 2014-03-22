5f311a8e33c09ee405f2c7f971ab84ea8234b3da
soh-i <soh.ishiguro3914@gmail.com>
2014-03-22 16:52:22 +0900

    Fix

--- src/Ivy/commandline2/load_settings.py
+++ src/Ivy/commandline2/load_settings.py
@@ -4,15 +4,20 @@ from Ivy.commandline2.settings import IVY_SETTINGS, EDIT_BENCH_SETTINGS
 
 class Setting(object):
     def __init__(self):
-        pass
+        self.ivy = IVY_SETTINGS
+        self.edit_bench = EDIT_BENCH_SETTINGS
         
     def load(self, cls):
         if cls == 'IVY_SETTINGS':
-            return IVY_SETTINGS
+            return self.ivy
         elif cls == 'EDIT_BENCH_SETTINGS':
-            return EDIT_BENCH_SETTINGS
+            return self.edit_bench
         
 if __name__ == '__main__':
     setting = Setting()
-    print setting.load('EDIT_BENCH_SETTINGS')
+    conf =  setting.load('IVY_SETTINGS')
+    print conf
+    
+    
+
     
